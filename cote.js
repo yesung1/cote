728f70f687a8c8906046912980e9403e41bf4acd
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 10:58:38 +0900

    add : 문자열 겹쳐쓰기

--- cote.js
+++ cote.js
@@ -1,23 +1,19 @@
 /*
 
 문제 설명
-자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+문자열 my_string, overwrite_string과 정수 s가 주어집니다. 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ n ≤ 1,000
+my_string와 overwrite_string은 숫자와 알파벳으로 이루어져 있습니다.
+1 ≤ overwrite_string의 길이 ≤ my_string의 길이 ≤ 1,000
+0 ≤ s ≤ my_string의 길이 - overwrite_string의 길이
+
 
 */
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let input = [];
-
-rl.on("line", function (line) {
-  input = line.split(" ");
-}).on("close", function () {
-  n = Number(input[0]);
-  console.log(n % 2 === 0 ? `${n} is even` : `${n} is odd`);
-});
+function solution(my_string, overwrite_string, s) {
+  let answer =
+    my_string.slice(0, s) +
+    overwrite_string +
+    my_string.slice(s + overwrite_string.length);
+  return answer;
+}
