894008c2770d4c4d71f79d9dbc03601554aa63f2
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:15:40 +0900

    add:카운트 업

--- cote.js
+++ cote.js
@@ -1,22 +1,19 @@
 /*
 
 문제 설명
-정수 l과 r이 주어졌을 때, l 이상 r이하의 정수 중에서 숫자 "0"과 "5"로만 이루어진 모든 정수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
-
-만약 그러한 정수가 없다면, -1이 담긴 배열을 return 합니다.
+정수 start와 end가 주어질 때, start부터 end까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ l ≤ r ≤ 1,000,000
+0 ≤ start ≤ end ≤ 50
 
 
 
 */
-function solution(l, r) {
+function solution(start, end) {
   var answer = [];
-  for (let x = l; l <= r; l++) {
-    l = l.toString();
-    if (![...l].every((x) => x === "0" || x === "5")) continue;
-    answer.push(l ? Number(l) : -1);
+  let x = start;
+  for (x; x <= end; x++) {
+    answer.push(x);
   }
-  return answer.length ? answer : [-1];
+  return answer;
 }
