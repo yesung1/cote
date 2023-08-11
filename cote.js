4d4d0de6ef64861aff95e1737fbf778453de2247
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 10:51:58 +0900

    add : 문자열 출력하기

---
+++ cote.js
@@ -0,0 +1,25 @@
+/*
+
+문제 설명
+문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+
+제한사항
+1 ≤ str의 길이 ≤ 1,000,000
+str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다.
+
+*/
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+  console.log(str);
+});
