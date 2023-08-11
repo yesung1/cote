4d52080d2d65bb489d6a2509ebd687eabc40ad04
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:20:00 +0900

    add:부분 문자열 이어 붙여 문자열 만들기

--- cote.js
+++ cote.js
@@ -1,24 +1,21 @@
 /*
 
 문제 설명
-문자열 배열 intStrs와 정수 k, s, l가 주어집니다. intStrs의 원소는 숫자로 이루어져 있습니다.
-
-배열 intStrs의 각 원소마다 s번 인덱스에서 시작하는 길이 l짜리 부분 문자열을 잘라내 정수로 변환합니다. 이때 변환한 정수값이 k보다 큰 값들을 담은 배열을 return 하는 solution 함수를 완성해 주세요.
+길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어집니다. parts[i]는 [s, e] 형태로, my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미합니다. 각 my_strings의 원소의 parts에 해당하는 부분 문자열을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-0 ≤ s < 100
-1 ≤ l ≤ 8
-10l - 1 ≤ k < 10l
-1 ≤ intStrs의 길이 ≤ 10,000
-s + l ≤ intStrs의 원소의 길이 ≤ 120
+1 ≤ my_strings의 길이 = parts의 길이 ≤ 100
+1 ≤ my_strings의 원소의 길이 ≤ 100
+parts[i]를 [s, e]라 할 때, 다음을 만족합니다.
+0 ≤ s ≤ e < my_strings[i]의 길이
 
 */
-function solution(intStrs, k, s, l) {
-  var answer = [];
-  let strVal = [];
-  intStrs.filter((str) => {
-    strVal = str.split("").splice(s, l).join("");
-    strVal > k ? answer.push(Number(strVal)) : "";
+function solution(my_strings, parts) {
+  var answer = "";
+  let val = "";
+  parts.map(([to, from], i) => {
+    val = my_strings[i].split("");
+    answer += val.slice(to, from + 1).join("");
   });
   return answer;
 }
