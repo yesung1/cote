d5f0b33525dba2b0b4b50f6ba0a295b49188ec24
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:22:57 +0900

    add:qr code

--- cote.js
+++ cote.js
@@ -1,22 +1,14 @@
 /*
 
+
 문제 설명
-문자열 my_string과 두 정수 m, c가 주어집니다. my_string을 한 줄에 m 글자씩 가로로 적었을 때 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
+두 정수 q, r과 문자열 code가 주어질 때, code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-my_string은 영소문자로 이루어져 있습니다.
-1 ≤ m ≤ my_string의 길이 ≤ 1,000
-m은 my_string 길이의 약수로만 주어집니다.
-1 ≤ c ≤ m
+0 ≤ r < q ≤ 20
+r < code의 길이 ≤ 1,000
+code는 영소문자로만 이루어져 있습니다.
 
 */
-function solution(my_string, m, c) {
-  var answer = "";
-  let strArr = "";
-  myLeng = my_string.length;
-  for (let x = 0; x < myLeng; x += m) {
-    strArr = my_string.substring(x, x + m);
-    answer += [...strArr][c - 1];
-  }
-  return answer;
-}
+const solution = (q, r, code) =>
+  [...code].filter((v, i) => i % q === r).join("");
