9697e075913f1e6052329aacd7bc2e2b2b186637
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 12:24:00 +0900

    add:카운트 다운

--- cote.js
+++ cote.js
@@ -1,14 +1,19 @@
 /*
 
 문제 설명
-문자열 my_string과 정수 배열 indices가 주어질 때, my_string에서 indices의 원소에 해당하는 인덱스의 글자를 지우고 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+정수 start와 end가 주어질 때, start에서 end까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ indices의 길이 < my_string의 길이 ≤ 100
-my_string은 영소문자로만 이루어져 있습니다
-0 ≤ indices의 원소 < my_string의 길이
-indices의 원소는 모두 서로 다릅니다.
+0 ≤ end ≤ start ≤ 50
+입출력 예
+start	end	result
+10	3	[10, 9, 8, 7, 6, 5, 4, 3]
 
 */
-const solution = (my_string, indices) =>
-  [...my_string].filter((str, i) => !indices.includes(i)).join("");
+function solution(start, end) {
+  var answer = [];
+  for (let x = start; x >= end; x--) {
+    answer.push(x);
+  }
+  return answer;
+}
