35951c30cccd3dbc7ebe339833421fe32aa0250d
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 10:55:46 +0900

    add : 특수문자 출력하기

--- cote.js
+++ cote.js
@@ -1,11 +1,11 @@
 /*
 
 문제 설명
-영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+다음과 같이 출력하도록 코드를 작성해 주세요.
 
-제한사항
-1 ≤ str의 길이 ≤ 20
-str은 알파벳으로 이루어진 문자열입니다.
+출력 예시
+
+!@#$%^&*(\'"<>?:;
 
 
 */
@@ -16,18 +16,6 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let input = [];
-let arr2 = [];
-let arr3 = [];
-
-rl.on("line", function (line) {
-  input = [line];
-}).on("close", function () {
-  str = input[0];
-  arr2 = str.split("");
-  arr3 = arr2.filter(function (e, i, arr) {
-    e === e.toUpperCase() ? e.toLowerCase() : e.toUpperCase();
-    console.log((arr[i] = e));
-  });
-  console.log(arr3.join(""));
+rl.on("close", function () {
+  console.log(`!@#$%^&*(\\'"<>?:;`);
 });
