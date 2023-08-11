804344d018e11e1314528c4a72c2dfe9d6163319
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 14:45:44 +0900

    add:가까운 1 찾기

--- cote.js
+++ cote.js
@@ -1,19 +1,20 @@
 /*
 
 문제 설명
-정수 start와 end가 주어질 때, start에서 end까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+정수 배열 arr가 주어집니다. 이때 arr의 원소는 1 또는 0입니다. 정수 idx가 주어졌을 때, idx보다 크면서 배열의 값이 1인 가장 작은 인덱스를 찾아서 반환하는 solution 함수를 완성해 주세요.
+
+단, 만약 그러한 인덱스가 없다면 -1을 반환합니다.
 
 제한사항
-0 ≤ end ≤ start ≤ 50
-입출력 예
-start	end	result
-10	3	[10, 9, 8, 7, 6, 5, 4, 3]
+3 ≤ arr의 길이 ≤ 100'000
+arr의 원소는 전부 1 또는 0입니다.
 
 */
-function solution(start, end) {
-  var answer = [];
-  for (let x = start; x >= end; x--) {
-    answer.push(x);
+function solution(arr, idx) {
+  var answer = 0;
+  for (let i = idx; i < arr.length; i++) {
+    arr[i] === 1 ? (answer = i) : (answer = -1);
+    if (answer >= 0) break;
   }
   return answer;
 }
