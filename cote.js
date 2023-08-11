4f7c53039f6346f84d42378672a33174c685fd67
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:04:17 +0900

    add:조건 문자열

--- cote.js
+++ cote.js
@@ -1,21 +1,21 @@
 /*
 문제 설명
-양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고 n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
+문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
 
-제한사항
-1 ≤ n ≤ 100
+두 수가 n과 m이라면
+">", "=" : n >= m
+"<", "=" : n <= m
+">", "!" : n > m
+"<", "!" : n < m
+두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
 
+제한 사항
+1 ≤ n, m ≤ 100
 */
-function solution(n) {
+function solution(ineq, eq, n, m) {
   var answer = 0;
-  let odd = 0;
-  let even = 0;
-  for (let x = 1; x < n + 1; x++) {
-    if (x % 2 === 1) {
-      odd += x;
-    } else {
-      even += x * x;
-    }
-  }
-  return n % 2 === 1 ? odd : even;
+  eq === "=" ? (eq = "=") : (eq = "");
+  console.log(eq);
+  const sum = new Function("a", "b", "return a " + ineq + eq + "b");
+  return (answer = sum(n, m) ? 1 : 0);
 }
