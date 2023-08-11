e8331a01f6672dc60921873d80f4dc61b92bc242
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:21:33 +0900

    add:문자열 뒤집기

--- cote.js
+++ cote.js
@@ -1,17 +1,20 @@
 /*
 
 문제 설명
-어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다. 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
-문자열 my_string과 is_suffix가 주어질 때, is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
+문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 100
-1 ≤ is_suffix의 길이 ≤ 100
-my_string과 is_suffix는 영소문자로만 이루어져 있습니다.
+my_string은 숫자와 알파벳으로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+0 ≤ s ≤ e < my_string의 길이
 
 */
-function solution(my_string, is_suffix) {
-  var answer = 0;
-  answer = my_string.endsWith(is_suffix) ? 1 : 0;
-  return answer;
+function solution(my_string, s, e) {
+  let myArr = [...my_string];
+  strVal = myArr
+    .slice(s, e + 1)
+    .reverse()
+    .join("");
+  myArr.splice(s, e - s + 1, strVal);
+  return myArr.join("");
 }
