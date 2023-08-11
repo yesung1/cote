a73f4ec2b39df8109cce2900c00576e9d6b298b5
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:21:10 +0900

    add:접미사인지 확인하기

--- cote.js
+++ cote.js
@@ -2,20 +2,16 @@
 
 문제 설명
 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다. 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
-문자열 my_string이 매개변수로 주어질 때, my_string의 모든 접미사를 사전순으로 정렬한 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+문자열 my_string과 is_suffix가 주어질 때, is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-my_string은 알파벳 소문자로만 이루어져 있습니다.
 1 ≤ my_string의 길이 ≤ 100
+1 ≤ is_suffix의 길이 ≤ 100
+my_string과 is_suffix는 영소문자로만 이루어져 있습니다.
 
 */
-function solution(my_string) {
-  const strLen = my_string.length;
-  return [...my_string]
-    .reduce((acc, cur, idx) => {
-      console.log(acc, cur, idx, strLen);
-      const curStr = my_string.slice(idx, strLen);
-      return [...acc, curStr];
-    }, [])
-    .sort();
+function solution(my_string, is_suffix) {
+  var answer = 0;
+  answer = my_string.endsWith(is_suffix) ? 1 : 0;
+  return answer;
 }
