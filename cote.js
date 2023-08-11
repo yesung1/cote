e69ac034eda9e3f6e901ef8cdd52c3ae4da166c5
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:09:23 +0900

    add:이어 붙인 수

--- cote.js
+++ cote.js
@@ -1,20 +1,24 @@
 /*
 
+
 문제 설명
-정수가 담긴 리스트 num_list가 주어질 때, 모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해주세요.
+정수가 담긴 리스트 num_list가 주어집니다. num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
 2 ≤ num_list의 길이 ≤ 10
 1 ≤ num_list의 원소 ≤ 9
+num_list에는 적어도 한 개씩의 짝수와 홀수가 있습니다.
+
 
 */
 function solution(num_list) {
-  let answer = 1;
-  let plus = 0;
-  [...num_list].map((li) => {
-    answer *= li;
-    plus += li;
+  let odd = 0;
+  let even = 0;
+  var answer = 0;
+  [...num_list].map((n) => {
+    n = n.toString();
+    n % 2 === 0 ? (odd += n) : (even += n);
   });
-  plus = plus ** 2;
-  return answer < plus ? 1 : 0;
+  answer = Number(odd) + Number(even);
+  return answer;
 }
