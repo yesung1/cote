d2f034bb0455e0d61dfec8f4b5b67c862b3c95de
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:07:13 +0900

    add:등차수열의 특정한 항만 더하기

--- cote.js
+++ cote.js
@@ -1,38 +1,21 @@
 /*
 
 문제 설명
-문자열 code가 주어집니다.
-code를 앞에서부터 읽으면서 만약 문자가 "1"이면 mode를 바꿉니다. mode에 따라 code를 읽어가면서 문자열 ret을 만들어냅니다.
-
-mode는 0과 1이 있으며, idx를 0 부터 code의 길이 - 1 까지 1씩 키워나가면서 code[idx]의 값에 따라 다음과 같이 행동합니다.
-
-mode가 0일 때
-code[idx]가 "1"이 아니면 idx가 짝수일 때만 ret의 맨 뒤에 code[idx]를 추가합니다.
-code[idx]가 "1"이면 mode를 0에서 1로 바꿉니다.
-mode가 1일 때
-code[idx]가 "1"이 아니면 idx가 홀수일 때만 ret의 맨 뒤에 code[idx]를 추가합니다.
-code[idx]가 "1"이면 mode를 1에서 0으로 바꿉니다.
-문자열 code를 통해 만들어진 문자열 ret를 return 하는 solution 함수를 완성해 주세요.
-
-단, 시작할 때 mode는 0이며, return 하려는 ret가 만약 빈 문자열이라면 대신 "EMPTY"를 return 합니다.
+두 정수 a, d와 길이가 n인 boolean 배열 included가 주어집니다. 첫째항이 a, 공차가 d인 등차수열에서 included[i]가 i + 1항을 의미할 때, 이 등차수열의 1항부터 n항까지 included가 true인 항들만 더한 값을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ code의 길이 ≤ 100,000
-code는 알파벳 소문자 또는 "1"로 이루어진 문자열입니다.
+1 ≤ a ≤ 100
+1 ≤ d ≤ 100
+1 ≤ included의 길이 ≤ 100
+included에는 true가 적어도 하나 존재합니다.
+
 
 */
-function solution(code) {
-  let mode = true;
-  let ret = "";
-  for (let x in code) {
-    if (!mode) {
-      x % 2 === 1 && code[x] !== "1" ? (ret += code[x]) : "";
-      code[x] === "1" ? (mode = true) : "";
-    } else {
-      x % 2 === 0 && code[x] !== "1" ? (ret += code[x]) : "";
-      code[x] === "1" ? (mode = false) : "";
-    }
-  }
-  let answer = ret === "" ? "EMPTY" : ret;
+function solution(a, d, included) {
+  var answer = 0;
+  [...included].reduce((acc, curr) => {
+    curr === true ? (answer += a) : curr;
+    a += d;
+  }, 0);
   return answer;
 }
