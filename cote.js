f3fa3b28d68144c62df7da387ef15072b684a759
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:02:48 +0900

    add:공배수

--- cote.js
+++ cote.js
@@ -1,16 +1,14 @@
 /*
-
 문제 설명
-정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+정수 number와 n, m이 주어집니다. number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-2 ≤ num ≤ 100
-2 ≤ n ≤ 9
+10 ≤ number ≤ 100
+2 ≤ n, m < 10
 
 */
-function solution(num, n) {
-  var answer = 0;
-
-  answer = num % n === 0 ? 1 : 0;
-  return answer;
+function solution(number, n, m) {
+  return Number(number) % Number(n) === 0 && Number(number) % Number(m) === 0
+    ? 1
+    : 0;
 }
