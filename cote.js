2599826bdd2d3f4c3a525ec759882df92ccbd332
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:01:55 +0900

    add:두 수의 연산값 비교하기

--- cote.js
+++ cote.js
@@ -5,11 +5,14 @@
 
 12 ⊕ 3 = 123
 3 ⊕ 12 = 312
-양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
 
-단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+
+제한사항
+1 ≤ a, b < 10,000
 
 */
 function solution(a, b) {
-  return Math.max(Number(`${a}${b}`), Number(`${b}${a}`));
+  return Math.max(Number(`${a}${b}`), Number(2 * a * b));
 }
