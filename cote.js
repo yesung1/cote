47d1e0f8eb2e9ed55ac381b13e298e76d650099e
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:18:43 +0900

    add:9로 나눈 나머지

--- cote.js
+++ cote.js
@@ -1,24 +1,17 @@
 /*
 
 문제 설명
-문자열 my_string과 정수 배열 index_list가 매개변수로 주어집니다. my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
+이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 1,000
-my_string의 원소는 영소문자로 이루어져 있습니다.
-1 ≤ index_list의 길이 ≤ 1,000
-0 ≤ index_list의 원소 < my_string의 길이
-
-
+1 ≤ number의 길이 ≤ 100,000
+number의 원소는 숫자로만 이루어져 있습니다.
+number는 정수 0이 아니라면 숫자 '0'으로 시작하지 않습니다.
 
 */
-function solution(my_string, index_list) {
-    var answer = '';
-    index_list.map((n)=>{
-        answer+=[...my_string][n];
-    })
-    return answer;
-}
-
-  return numFrequency;
+function solution(number) {
+  var answer = 0;
+  answer = BigInt(number) % 9n;
+  return answer;
 }
