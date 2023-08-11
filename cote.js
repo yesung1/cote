81bfb736f9b166fdc3f88b77777bc6f93f547119
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:23:29 +0900

    add:문자 개수 세기

--- cote.js
+++ cote.js
@@ -2,13 +2,18 @@
 
 
 문제 설명
-두 정수 q, r과 문자열 code가 주어질 때, code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때, my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, my_string에서 'a'의 개수, my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-0 ≤ r < q ≤ 20
-r < code의 길이 ≤ 1,000
-code는 영소문자로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
 
 */
-const solution = (q, r, code) =>
-  [...code].filter((v, i) => i % q === r).join("");
+function solution(my_string) {
+  var answer = [];
+  answer = new Array(52).fill(0);
+  let al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < my_string.length; i++) {
+    answer[al.indexOf(my_string[i])]++;
+  }
+  return answer;
+}
