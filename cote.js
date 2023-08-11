81bb4bb3e556518ce24cd1e5ac27489d6ab8cb81
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:09:55 +0900

    add:마지막 두 원소

--- cote.js
+++ cote.js
@@ -2,23 +2,19 @@
 
 
 문제 설명
-정수가 담긴 리스트 num_list가 주어집니다. num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해주세요.
+정수 리스트 num_list가 주어질 때, 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여 return하도록 solution 함수를 완성해주세요.
 
 제한사항
 2 ≤ num_list의 길이 ≤ 10
 1 ≤ num_list의 원소 ≤ 9
-num_list에는 적어도 한 개씩의 짝수와 홀수가 있습니다.
 
 
 */
 function solution(num_list) {
-  let odd = 0;
-  let even = 0;
-  var answer = 0;
-  [...num_list].map((n) => {
-    n = n.toString();
-    n % 2 === 0 ? (odd += n) : (even += n);
-  });
-  answer = Number(odd) + Number(even);
-  return answer;
+  let lastOne = num_list.slice(-1);
+  let lastTwo = num_list.slice(-2, -1);
+  lastOne > lastTwo
+    ? num_list.push(lastOne - lastTwo)
+    : num_list.push(lastOne * 2);
+  return num_list;
 }
