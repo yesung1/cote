7519bd560489c586543c1b462a3f8e647602b25a
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:15:15 +0900

    add:배열 만들기 2

--- cote.js
+++ cote.js
@@ -1,28 +1,22 @@
 /*
 
 문제 설명
-정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [s, e, k] 꼴입니다.
+정수 l과 r이 주어졌을 때, l 이상 r이하의 정수 중에서 숫자 "0"과 "5"로만 이루어진 모든 정수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
 
-각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 i가 k의 배수이면 arr[i]에 1을 더합니다.
-
-위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+만약 그러한 정수가 없다면, -1이 담긴 배열을 return 합니다.
 
 제한사항
-1 ≤ arr의 길이 ≤ 1,000
-0 ≤ arr의 원소 ≤ 1,000,000
-1 ≤ queries의 길이 ≤ 1,000
-0 ≤ s ≤ e < arr의 길이
-0 ≤ k ≤ 5
+1 ≤ l ≤ r ≤ 1,000,000
 
 
 
 */
-function solution(arr, queries) {
-  let answer = [];
-  for (let [s, e, k] of queries) {
-    answer.push(
-      arr.map((v, i) => (s <= i && i <= e && i % k === 0 ? arr[i]++ : v))
-    );
+function solution(l, r) {
+  var answer = [];
+  for (let x = l; l <= r; l++) {
+    l = l.toString();
+    if (![...l].every((x) => x === "0" || x === "5")) continue;
+    answer.push(l ? Number(l) : -1);
   }
-  return arr;
+  return answer.length ? answer : [-1];
 }
