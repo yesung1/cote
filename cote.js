13e95fe08591eb93fc4932a77f573cda30775818
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:10:39 +0900

    add:수 조작하기 1

--- cote.js
+++ cote.js
@@ -2,19 +2,40 @@
 
 
 문제 설명
-정수 리스트 num_list가 주어질 때, 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여 return하도록 solution 함수를 완성해주세요.
+정수 n과 문자열 control이 주어집니다. control은 "w", "a", "s", "d"의 4개의 문자로 이루어져 있으며, control의 앞에서부터 순서대로 문자에 따라 n의 값을 바꿉니다.
+
+"w" : n이 1 커집니다.
+"s" : n이 1 작아집니다.
+"d" : n이 10 커집니다.
+"a" : n이 10 작아집니다.
+위 규칙에 따라 n을 바꿨을 때 가장 마지막에 나오는 n의 값을 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
-2 ≤ num_list의 길이 ≤ 10
-1 ≤ num_list의 원소 ≤ 9
+-100,000 ≤ n ≤ 100,000
+1 ≤ control의 길이 ≤ 100,000
+control은 알파벳 소문자 "w", "a", "s", "d"로 이루어진 문자열입니다.
+
 
 
 */
-function solution(num_list) {
-  let lastOne = num_list.slice(-1);
-  let lastTwo = num_list.slice(-2, -1);
-  lastOne > lastTwo
-    ? num_list.push(lastOne - lastTwo)
-    : num_list.push(lastOne * 2);
-  return num_list;
+function solution(n, control) {
+  var answer = 0;
+  function func1(item) {
+    switch (item) {
+      case "w":
+        n += 1;
+        break;
+      case "s":
+        n -= 1;
+        break;
+      case "d":
+        n += 10;
+        break;
+      case "a":
+        n -= 10;
+        break;
+    }
+  }
+  [...control].map((item, i) => func1(item));
+  return n;
 }
