c0a73b848e81901d3eb2962fa0ab6ac37205a075
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 12:21:46 +0900

    add:글자 지우기

--- cote.js
+++ cote.js
@@ -1,17 +1,14 @@
 /*
 
 문제 설명
-정수 n과 k가 주어졌을 때, 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+문자열 my_string과 정수 배열 indices가 주어질 때, my_string에서 indices의 원소에 해당하는 인덱스의 글자를 지우고 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ n ≤ 1,000,000
-1 ≤ k ≤ min(1,000, n)
+1 ≤ indices의 길이 < my_string의 길이 ≤ 100
+my_string은 영소문자로만 이루어져 있습니다
+0 ≤ indices의 원소 < my_string의 길이
+indices의 원소는 모두 서로 다릅니다.
 
 */
-function solution(n, k) {
-  var answer = [];
-  for (let i = k; i <= n; i += k) {
-    answer.push(i);
-  }
-  return answer;
-}
+const solution = (my_string, indices) =>
+  [...my_string].filter((str, i) => !indices.includes(i)).join("");
