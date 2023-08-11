972d773489f04490610752fe3ad609b453169482
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:14:35 +0900

    add:수열과 구간 쿼리 4

--- cote.js
+++ cote.js
@@ -1,34 +1,28 @@
 /*
 
-
 문제 설명
 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [s, e, k] 꼴입니다.
 
-각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 k보다 크면서 가장 작은 arr[i]를 찾습니다.
+각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 i가 k의 배수이면 arr[i]에 1을 더합니다.
 
-각 쿼리의 순서에 맞게 답을 저장한 배열을 반환하는 solution 함수를 완성해 주세요.
-단, 특정 쿼리의 답이 존재하지 않으면 -1을 저장합니다.
+위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
 1 ≤ arr의 길이 ≤ 1,000
 0 ≤ arr의 원소 ≤ 1,000,000
 1 ≤ queries의 길이 ≤ 1,000
 0 ≤ s ≤ e < arr의 길이
-0 ≤ k ≤ 1,000,000
+0 ≤ k ≤ 5
 
 
 
 */
 function solution(arr, queries) {
-  var answer = [];
-  let value = 0;
+  let answer = [];
   for (let [s, e, k] of queries) {
-    value = arr
-      .filter((v, i) => s <= i && i <= e && v > k)
-      .sort((a, b) => a - b)[0];
-    console.log(value);
-    value = !value ? -1 : value;
-    answer.push(value);
+    answer.push(
+      arr.map((v, i) => (s <= i && i <= e && i % k === 0 ? arr[i]++ : v))
+    );
   }
-  return answer;
+  return arr;
 }
