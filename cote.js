30e2436570cb862fe39554cc2a67c28f956ae92b
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 10:53:41 +0900

    add : 문자열 반복해서 출력하기

--- cote.js
+++ cote.js
@@ -1,10 +1,12 @@
 /*
 
 문제 설명
-정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+문자열 str과 정수 n이 주어집니다.
+str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
 
 제한사항
--100,000 ≤ a, b ≤ 100,000
+1 ≤ str의 길이 ≤ 10
+1 ≤ n ≤ 5
 
 */
 
@@ -19,6 +21,11 @@ let input = [];
 rl.on("line", function (line) {
   input = line.split(" ");
 }).on("close", function () {
-  console.log("a = " + Number(input[0]));
-  console.log("b = " + Number(input[1]));
+  str = input[0];
+  n = Number(input[1]);
+  let strSum = "";
+  for (let i = 0; i < n; i++) {
+    strSum += str;
+  }
+  console.log(strSum);
 });
