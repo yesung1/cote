dc713a6f7433799e782435f4250694fd7d6cafbc
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-11 11:05:15 +0900

    add:flag에 따라 다른 값 반환하기

--- cote.js
+++ cote.js
@@ -1,21 +1,15 @@
 /*
+
 문제 설명
-문자열에 따라 다음과 같이 두 수의 크기를 비교하려고 합니다.
+두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
 
-두 수가 n과 m이라면
-">", "=" : n >= m
-"<", "=" : n <= m
-">", "!" : n > m
-"<", "!" : n < m
-두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때, n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
+제한사항
+-1,000 ≤ a, b ≤ 1,000
 
-제한 사항
-1 ≤ n, m ≤ 100
 */
-function solution(ineq, eq, n, m) {
-  var answer = 0;
-  eq === "=" ? (eq = "=") : (eq = "");
-  console.log(eq);
-  const sum = new Function("a", "b", "return a " + ineq + eq + "b");
-  return (answer = sum(n, m) ? 1 : 0);
+function solution(a, b, flag) {
+  flag === true ? (flag = "+") : (flag = "-");
+  const sum = new Function("a", "b", "return a" + flag + "b");
+  let answer = sum(a, b);
+  return answer;
 }
