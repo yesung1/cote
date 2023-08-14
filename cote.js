029179cc42669e3ec2fc4046492cd3ba6e0c8576
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-14 10:59:20 +0900

    add:배열 만들기 3

--- cote.js
+++ cote.js
@@ -1,12 +1,24 @@
 /*
 
 문제 설명
-정수 리스트 num_list가 주어질 때, 첫 번째로 나오는 음수의 인덱스를 return하도록 solution 함수를 완성해주세요. 음수가 없다면 -1을 return합니다.
+정수 배열 arr와 2개의 구간이 담긴 배열 intervals가 주어집니다.
+
+intervals는 항상 [[a1, b1], [a2, b2]]의 꼴로 주어지며 각 구간은 닫힌 구간입니다. 닫힌 구간은 양 끝값과 그 사이의 값을 모두 포함하는 구간을 의미합니다.
+
+이때 배열 arr의 첫 번째 구간에 해당하는 배열과 두 번째 구간에 해당하는 배열을 앞뒤로 붙여 새로운 배열을 만들어 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
-5 ≤ num_list의 길이 ≤ 100
--10 ≤ num_list의 원소 ≤ 100
+1 ≤ arr의 길이 ≤ 100,000
+1 ≤ arr의 원소 < 100
+1 ≤ a1 ≤ b1 < arr의 길이
+1 ≤ a2 ≤ b2 < arr의 길이
 
 */
 
-const solution = (num_list) => num_list.findIndex((n) => n < 0);
+function solution(arr, intervals) {
+  var answer = [];
+  intervals.map(([start, end]) => {
+    answer.push(arr.slice(start, end + 1));
+  });
+  return answer.flat();
+}
