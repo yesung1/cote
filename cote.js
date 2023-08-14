0b09f7ad0d640ed1ce3303f9359fbd09590dcbfb
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-14 12:20:45 +0900

    add:순서 바꾸기

--- cote.js
+++ cote.js
@@ -1,7 +1,8 @@
 /*
 
+순서 바꾸기
 문제 설명
-정수 리스트 num_list와 정수 n이 주어질 때, n 번째 원소부터 마지막 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+정수 리스트 num_list와 정수 n이 주어질 때, num_list를 n 번째 원소 이후의 원소들과 n 번째까지의 원소들로 나눠 n 번째 원소 이후의 원소들을 n 번째까지의 원소들 앞에 붙인 리스트를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
 2 ≤ num_list의 길이 ≤ 30
@@ -9,6 +10,11 @@
 1 ≤ n ≤ num_list의 길이
 
 
+
 */
 
-const solution = (num_list, n) => num_list.splice(n - 1, num_list.length);
+function solution(num_list, n) {
+  let first = num_list.slice(0, n);
+  let last = num_list.slice(n, num_list.length);
+  return [...last, ...first];
+}
