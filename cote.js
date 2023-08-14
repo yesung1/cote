36315686c91e842234255a3bc1fd65d57697ad22
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-14 11:35:07 +0900

    add:2의 영역

--- cote.js
+++ cote.js
@@ -1,24 +1,24 @@
 /*
 
 문제 설명
-정수 배열 arr와 2개의 구간이 담긴 배열 intervals가 주어집니다.
+정수 배열 arr가 주어집니다. 배열 안의 2가 모두 포함된 가장 작은 연속된 부분 배열을 return 하는 solution 함수를 완성해 주세요.
 
-intervals는 항상 [[a1, b1], [a2, b2]]의 꼴로 주어지며 각 구간은 닫힌 구간입니다. 닫힌 구간은 양 끝값과 그 사이의 값을 모두 포함하는 구간을 의미합니다.
-
-이때 배열 arr의 첫 번째 구간에 해당하는 배열과 두 번째 구간에 해당하는 배열을 앞뒤로 붙여 새로운 배열을 만들어 return 하는 solution 함수를 완성해 주세요.
+단, arr에 2가 없는 경우 [-1]을 return 합니다.
 
 제한사항
 1 ≤ arr의 길이 ≤ 100,000
-1 ≤ arr의 원소 < 100
-1 ≤ a1 ≤ b1 < arr의 길이
-1 ≤ a2 ≤ b2 < arr의 길이
+1 ≤ arr의 원소 ≤ 10
 
 */
 
-function solution(arr, intervals) {
+function solution(arr) {
   var answer = [];
-  intervals.map(([start, end]) => {
-    answer.push(arr.slice(start, end + 1));
-  });
-  return answer.flat();
+  let first = 0;
+  let last = 0;
+  first = arr.findIndex((a) => a === 2);
+  last = arr.length - [...arr].reverse().findIndex((a) => a === 2) - 1;
+  first === -1 || last === -1
+    ? (answer = [-1])
+    : (answer = arr.slice(first, last + 1));
+  return answer;
 }
