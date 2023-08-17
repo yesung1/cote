dac84b7c8919bc456b6d9eac4712d8daec8beeac
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-17 14:13:42 +0900

    add:홀수 vs 짝수

--- cote.js
+++ cote.js
@@ -1,20 +1,20 @@
 /*
 
 문제 설명
-정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+정수 리스트 num_list가 주어집니다. 가장 첫 번째 원소를 1번 원소라고 할 때, 홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해주세요. 두 값이 같을 경우 그 값을 return합니다.
 
 제한사항
-5 ≤ num_list의 길이 ≤ 20
-1 ≤ num_list의 원소 ≤ 9
-1 ≤ n ≤ 4
+5 ≤ num_list의 길이 ≤ 50
+-9 ≤ num_list의 원소 ≤ 9
 
 
 */
 
-function solution(num_list, n) {
-  var answer = [];
-  answer = num_list.filter((_, i) => {
-    return i % n === 0;
+function solution(num_list) {
+  let odd = 0;
+  let even = 0;
+  num_list.filter((n, i) => {
+    i % 2 ? (odd += n) : (even += n);
   });
-  return answer;
+  return odd >= even ? odd : even;
 }
