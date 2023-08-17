cdf5e9d808a688b4e9634855a96abf51076cb20c
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-17 15:57:51 +0900

    add:5명씩

--- cote.js
+++ cote.js
@@ -1,20 +1,16 @@
 /*
 
 문제 설명
-정수 리스트 num_list가 주어집니다. 가장 첫 번째 원소를 1번 원소라고 할 때, 홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해주세요. 두 값이 같을 경우 그 값을 return합니다.
+최대 5명씩 탑승가능한 놀이기구를 타기 위해 줄을 서있는 사람들의 이름이 담긴 문자열 리스트 names가 주어질 때, 앞에서 부터 5명씩 묶은 그룹의 가장 앞에 서있는 사람들의 이름을 담은 리스트를 return하도록 solution 함수를 완성해주세요. 마지막 그룹이 5명이 되지 않더라도 가장 앞에 있는 사람의 이름을 포함합니다.
 
 제한사항
-5 ≤ num_list의 길이 ≤ 50
--9 ≤ num_list의 원소 ≤ 9
-
+5 ≤ names의 길이 ≤ 30
+1 ≤ names의 원소의 길이 ≤ 10
+names의 원소는 영어 알파벳 소문자로만 이루어져 있습니다.
 
 */
 
-function solution(num_list) {
-  let odd = 0;
-  let even = 0;
-  num_list.filter((n, i) => {
-    i % 2 ? (odd += n) : (even += n);
-  });
-  return odd >= even ? odd : even;
-}
+const solution = (names) =>
+  (answer = names.filter((n, i) => {
+    return i % 5 === 0;
+  }));
