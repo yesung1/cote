50bf5a4c6e25f5784a7c781906ea2c91ac6a9911
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-17 16:57:29 +0900

    add:n보다 커질 때까지 더하기

--- cote.js
+++ cote.js
@@ -1,20 +1,17 @@
 /*
 
 문제 설명
-오늘 해야 할 일이 담긴 문자열 배열 todo_list와 각각의 일을 지금 마쳤는지를 나타내는 boolean 배열 finished가 매개변수로 주어질 때, todo_list에서 아직 마치지 못한 일들을 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+정수 배열 numbers와 정수 n이 매개변수로 주어집니다. numbers의 원소를 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ todo_list의 길이 1 ≤ 100
-2 ≤ todo_list의 원소의 길이 ≤ 20
-todo_list의 원소는 영소문자로만 이루어져 있습니다.
-todo_list의 원소는 모두 서로 다릅니다.
-finished[i]는 true 또는 false이고 true는 todo_list[i]를 마쳤음을, false는 아직 마치지 못했음을 나타냅니다.
-아직 마치지 못한 일이 적어도 하나 있습니다.
-
+1 ≤ numbers의 길이 ≤ 100
+1 ≤ numbers의 원소 ≤ 100
+0 ≤ n < numbers의 모든 원소의 합
 
 */
 
-const solution = (todo_list, finished) =>
-  todo_list.filter((_, i) => {
-    return !finished[i];
-  });
+const solution = (numbers, n) =>
+  numbers.reduce((acc, curr) => {
+    if (acc > n) return acc;
+    return acc + curr;
+  }, 0);
