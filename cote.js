57478418efd697ca46eb1327141d7b7e5448c515
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-17 13:56:44 +0900

    add:n개 간격의 원소들

--- cote.js
+++ cote.js
@@ -1,19 +1,20 @@
 /*
 
 문제 설명
-정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-2 ≤ num_list의 길이 ≤ 30
+5 ≤ num_list의 길이 ≤ 20
 1 ≤ num_list의 원소 ≤ 9
-1 ≤ n ≤ num_list의 길이 ___
-
+1 ≤ n ≤ 4
 
 
 */
 
 function solution(num_list, n) {
   var answer = [];
-  answer = num_list.splice(0, n);
+  answer = num_list.filter((_, i) => {
+    return i % n === 0;
+  });
   return answer;
 }
