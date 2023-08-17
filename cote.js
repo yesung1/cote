da4a4a7c6279f9790e69822cd51888ba9941ec8e
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-17 16:07:24 +0900

    add:할 일 목록

--- cote.js
+++ cote.js
@@ -1,16 +1,20 @@
 /*
 
 문제 설명
-최대 5명씩 탑승가능한 놀이기구를 타기 위해 줄을 서있는 사람들의 이름이 담긴 문자열 리스트 names가 주어질 때, 앞에서 부터 5명씩 묶은 그룹의 가장 앞에 서있는 사람들의 이름을 담은 리스트를 return하도록 solution 함수를 완성해주세요. 마지막 그룹이 5명이 되지 않더라도 가장 앞에 있는 사람의 이름을 포함합니다.
+오늘 해야 할 일이 담긴 문자열 배열 todo_list와 각각의 일을 지금 마쳤는지를 나타내는 boolean 배열 finished가 매개변수로 주어질 때, todo_list에서 아직 마치지 못한 일들을 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-5 ≤ names의 길이 ≤ 30
-1 ≤ names의 원소의 길이 ≤ 10
-names의 원소는 영어 알파벳 소문자로만 이루어져 있습니다.
+1 ≤ todo_list의 길이 1 ≤ 100
+2 ≤ todo_list의 원소의 길이 ≤ 20
+todo_list의 원소는 영소문자로만 이루어져 있습니다.
+todo_list의 원소는 모두 서로 다릅니다.
+finished[i]는 true 또는 false이고 true는 todo_list[i]를 마쳤음을, false는 아직 마치지 못했음을 나타냅니다.
+아직 마치지 못한 일이 적어도 하나 있습니다.
+
 
 */
 
-const solution = (names) =>
-  (answer = names.filter((n, i) => {
-    return i % 5 === 0;
-  }));
+const solution = (todo_list, finished) =>
+  todo_list.filter((_, i) => {
+    return !finished[i];
+  });
