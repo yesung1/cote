17147457e8f2119b68e86a78e1e3085fbb389d16
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-18 17:47:54 +0900

    add:공백으로 구분하기 1

--- cote.js
+++ cote.js
@@ -1,17 +1,14 @@
 /*
 
 문제 설명
-알파벳으로 이루어진 문자열 myString과 pat이 주어집니다. myString의 연속된 부분 문자열 중 pat이 존재하면 1을 그렇지 않으면 0을 return 하는 solution 함수를 완성해 주세요.
-
-단, 알파벳 대문자와 소문자는 구분하지 않습니다.
+단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때, my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ myString의 길이 ≤ 100,000
-1 ≤ pat의 길이 ≤ 300
-myString과 pat은 모두 알파벳으로 이루어진 문자열입니다.
+my_string은 영소문자와 공백으로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+my_string의 맨 앞과 맨 뒤에 글자는 공백이 아닙니다.
 
 
 */
 
-const solution = (myString, pat) =>
-  myString.toLowerCase().indexOf(pat.toLowerCase()) === -1 ? 0 : 1;
+const solution = (my_string) => my_string.split(" ");
