2e58d9f4719a9bb47d40e098f56c2311b23c7b27
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-18 16:36:29 +0900

    add:1로 만들기

--- cote.js
+++ cote.js
@@ -1,33 +1,31 @@
 /*
 
 문제 설명
-정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱하고 다시 1을 더합니다.
+정수가 있을 때, 짝수라면 반으로 나누고, 홀수라면 1을 뺀 뒤 반으로 나누면, 마지막엔 1이 됩니다. 예를 들어 10이 있다면 다음과 같은 과정으로 1이 됩니다.
 
-이러한 작업을 x번 반복한 결과인 배열을 arr(x)라고 표현했을 때, arr(x) = arr(x + 1)인 x가 항상 존재합니다. 이러한 x 중 가장 작은 값을 return 하는 solution 함수를 완성해 주세요.
+10 / 2 = 5
+(5 - 1) / 2 = 4
+4 / 2 = 2
+2 / 2 = 1
+위와 같이 4번의 나누기 연산으로 1이 되었습니다.
 
-단, 두 배열에 대한 "="는 두 배열의 크기가 서로 같으며, 같은 인덱스의 원소가 각각 서로 같음을 의미합니다.
+정수들이 담긴 리스트 num_list가 주어질 때, num_list의 모든 원소를 1로 만들기 위해서 필요한 나누기 연산의 횟수를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ arr의 길이 ≤ 1,000,000
-1 ≤ arr의 원소의 값 ≤ 100
+3 ≤ num_list의 길이 ≤ 15
+1 ≤ num_list의 원소 ≤ 30
 
 
 */
 
-function solution(arr) {
-  let idx = 0;
-  let prevArr = arr;
-  while (true) {
-    const changeCurArr = prevArr.map((a) => {
-      if (a >= 50 && a % 2 === 0) return a / 2;
-      if (a < 50 && a % 2 === 1) return a * 2 + 1;
-      return a;
-    });
-    const isAllSame = prevArr.every((a, i) => a === changeCurArr[i]);
-    if (isAllSame) break;
-    idx += 1;
-    prevArr = changeCurArr;
-  }
-
-  return idx;
+function solution(num_list) {
+  let count = 0;
+  num_list.map((n) => {
+    while (true) {
+      if (n === 1) break;
+      count++;
+      n % 2 ? (n = (n - 1) / 2) : (n = n / 2);
+    }
+  });
+  return count;
 }
