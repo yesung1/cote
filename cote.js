7d3815e3536bdf2730367018b1948bfccba99ff1
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-18 10:04:48 +0900

    add:수열과 구간 쿼리 1

--- cote.js
+++ cote.js
@@ -1,17 +1,26 @@
 /*
 
 문제 설명
-정수 배열 numbers와 정수 n이 매개변수로 주어집니다. numbers의 원소를 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
+정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [s, e] 꼴입니다.
+
+각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 arr[i]에 1을 더합니다.
+
+위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
-1 ≤ numbers의 길이 ≤ 100
-1 ≤ numbers의 원소 ≤ 100
-0 ≤ n < numbers의 모든 원소의 합
+1 ≤ arr의 길이 ≤ 1,000
+0 ≤ arr의 원소 ≤ 1,000,000
+1 ≤ queries의 길이 ≤ 1,000
+0 ≤ s ≤ e < arr의 길이
 
 */
 
-const solution = (numbers, n) =>
-  numbers.reduce((acc, curr) => {
-    if (acc > n) return acc;
-    return acc + curr;
-  }, 0);
+function solution(arr, queries) {
+  var answer = [];
+  queries.map(([s, e]) => {
+    for (let i = s; i <= e; i++) {
+      arr[i] += 1;
+    }
+  });
+  return (answer = arr);
+}
