280771d052bcaa472f4fcb435d0598b2b2e9155c
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-18 17:00:27 +0900

    add:원하는 문자열 찾기

--- cote.js
+++ cote.js
@@ -1,18 +1,17 @@
 /*
 
 문제 설명
-정수가 담긴 리스트 num_list가 주어질 때, 리스트의 길이가 11 이상이면 리스트에 있는 모든 원소의 합을 10 이하이면 모든 원소의 곱을 return하도록 solution 함수를 완성해주세요.
+알파벳으로 이루어진 문자열 myString과 pat이 주어집니다. myString의 연속된 부분 문자열 중 pat이 존재하면 1을 그렇지 않으면 0을 return 하는 solution 함수를 완성해 주세요.
+
+단, 알파벳 대문자와 소문자는 구분하지 않습니다.
 
 제한사항
-2 ≤ num_list의 길이 ≤ 20
-1 ≤ num_list의 원소 ≤ 9
+1 ≤ myString의 길이 ≤ 100,000
+1 ≤ pat의 길이 ≤ 300
+myString과 pat은 모두 알파벳으로 이루어진 문자열입니다.
 
 
 */
 
-function solution(num_list) {
-  var answer = [...num_list];
-  return answer.length > 10
-    ? answer.reduce((acc, curr) => acc + curr)
-    : answer.reduce((acc, curr) => acc * curr);
-}
+const solution = (myString, pat) =>
+  myString.toLowerCase().indexOf(pat.toLowerCase()) === -1 ? 0 : 1;
