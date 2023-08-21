b9e8944b3bb97fd4d2d5ae12717a879956461710
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-21 18:31:43 +0900

    add:배열의 원소만큼 추가하기

--- cote.js
+++ cote.js
@@ -1,16 +1,23 @@
 /*
 
 문제 설명
-단어가 공백 한 개 이상으로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때, my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+아무 원소도 들어있지 않은 빈 배열 X가 있습니다. 양의 정수 배열 arr가 매개변수로 주어질 때, arr의 앞에서부터 차례대로 원소를 보면서 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-my_string은 영소문자와 공백으로만 이루어져 있습니다.
-1 ≤ my_string의 길이 ≤ 1,000
-my_string의 맨 앞과 맨 뒤에도 공백이 있을 수 있습니다.
-my_string에는 단어가 하나 이상 존재합니다.
+1 ≤ arr의 길이 ≤ 100
+1 ≤ arr의 원소 ≤ 100
 
 
 
 */
 
-const solution = (my_string) => my_string.split(/\s+/).filter((s) => s !== "");
+function solution(arr) {
+  var answer = [];
+  let v = 0;
+  arr.map((n, i) => {
+    for (let x = 0; x < arr[i]; x++) {
+      answer.push(arr[i]);
+    }
+  });
+  return answer;
+}
