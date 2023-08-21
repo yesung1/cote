1897a435602bc5906ecd2ce1f791503e3f3d6119
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-21 14:58:01 +0900

    add:공백으로 구분하기 1

--- cote.js
+++ cote.js
@@ -1,15 +1,15 @@
 /*
 
 문제 설명
-문자열 배열 strArr가 주어집니다. 배열 내의 문자열 중 "ad"라는 부분 문자열을 포함하고 있는 모든 문자열을 제거하고 남은 문자열을 순서를 유지하여 배열로 return 하는 solution 함수를 완성해 주세요.
+단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때, my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ strArr의 길이 ≤ 1,000
-1 ≤ strArr의 원소의 길이 ≤ 20
-strArr의 원소는 알파벳 소문자로 이루어진 문자열입니다.
+my_string은 영소문자와 공백으로만 이루어져 있습니다.
+1 ≤ my_string의 길이 ≤ 1,000
+my_string의 맨 앞과 맨 뒤에 글자는 공백이 아닙니다.
 
 
 
 */
 
-const solution = (strArr) => strArr.filter((str) => !str.includes("ad"));
+const solution = (my_string) => my_string.split(" ");
