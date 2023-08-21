ac7645da4860bcb76eb9cde237bf507c9917ebcb
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-21 14:33:20 +0900

    add:문자열이 몇 번 등장하는지 세기

--- cote.js
+++ cote.js
@@ -1,16 +1,23 @@
 /*
 
 문제 설명
-문자열 myString과 pat가 주어집니다. myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
+문자열 myString과 pat이 주어집니다. myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
-5 ≤ myString ≤ 20
-1 ≤ pat ≤ 5
-pat은 반드시 myString의 부분 문자열로 주어집니다.
-myString과 pat에 등장하는 알파벳은 대문자와 소문자를 구분합니다.
+1 ≤ myString ≤ 1000
+1 ≤ pat ≤ 10
+
 
 
 */
 
-const solution = (myString, pat) =>
-  myString.slice(0, myString.lastIndexOf(pat)) + pat;
+function solution(myString, pat) {
+  var answer = 0;
+  let matchStr = "";
+  answer = [...myString].reduce((acc, curr, i) => {
+    matchStr = myString.slice(i, pat.length + i);
+    if (matchStr === pat) acc++;
+    return acc;
+  }, 0);
+  return answer;
+}
