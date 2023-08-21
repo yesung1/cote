206972e8b560c5641c3c807fbe44a60fff13fa34
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-21 14:57:20 +0900

    add:ad 제거하기

--- cote.js
+++ cote.js
@@ -1,23 +1,15 @@
 /*
 
 문제 설명
-문자열 myString과 pat이 주어집니다. myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
+문자열 배열 strArr가 주어집니다. 배열 내의 문자열 중 "ad"라는 부분 문자열을 포함하고 있는 모든 문자열을 제거하고 남은 문자열을 순서를 유지하여 배열로 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
-1 ≤ myString ≤ 1000
-1 ≤ pat ≤ 10
+1 ≤ strArr의 길이 ≤ 1,000
+1 ≤ strArr의 원소의 길이 ≤ 20
+strArr의 원소는 알파벳 소문자로 이루어진 문자열입니다.
 
 
 
 */
 
-function solution(myString, pat) {
-  var answer = 0;
-  let matchStr = "";
-  answer = [...myString].reduce((acc, curr, i) => {
-    matchStr = myString.slice(i, pat.length + i);
-    if (matchStr === pat) acc++;
-    return acc;
-  }, 0);
-  return answer;
-}
+const solution = (strArr) => strArr.filter((str) => !str.includes("ad"));
