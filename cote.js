1141bf3b7fe5de73407f2d53a0d683f2f0b9b19f
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-21 10:33:53 +0900

    add:A 강조하기

--- cote.js
+++ cote.js
@@ -1,15 +1,21 @@
 /*
 
 문제 설명
-문자열 배열 strArr가 주어집니다. 모든 원소가 알파벳으로만 이루어져 있을 때, 배열에서 홀수번째 인덱스의 문자열은 모든 문자를 대문자로, 짝수번째 인덱스의 문자열은 모든 문자를 소문자로 바꿔서 반환하는 solution 함수를 완성해 주세요.
+문자열 myString이 주어집니다. myString에서 알파벳 "a"가 등장하면 전부 "A"로 변환하고, "A"가 아닌 모든 대문자 알파벳은 소문자 알파벳으로 변환하여 return 하는 solution 함수를 완성하세요.
 
 제한사항
-1 ≤ strArr ≤ 20
-1 ≤ strArr의 원소의 길이 ≤ 20
-strArr의 원소는 알파벳으로 이루어진 문자열 입니다.
+1 ≤ myString의 길이 ≤ 20
+myString은 알파벳으로 이루어진 문자열입니다.
 
 
 */
 
-const solution = (strArr) =>
-  strArr.map((arr, i) => (i % 2 === 0 ? arr.toLowerCase() : arr.toUpperCase()));
+function solution(myString) {
+  var answer = "";
+  answer = [...myString].map((v) => {
+    v = v.toLowerCase();
+    if (v === "a" || v === "A") v = v.toUpperCase();
+    return v;
+  });
+  return answer.join("");
+}
