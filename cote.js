cffefdc7785a513f620d42e91e119056cb7af6a5
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-21 10:27:29 +0900

    add:소문자로 바꾸기

--- cote.js
+++ cote.js
@@ -1,7 +1,7 @@
 /*
 
 문제 설명
-알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 대문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
 1 ≤ myString의 길이 ≤ 100,000
@@ -10,4 +10,4 @@ myString은 알파벳으로 이루어진 문자열입니다.
 
 */
 
-solution = (myString) => myString.toUpperCase();
+const solution = (myString) => myString.toLowerCase();
