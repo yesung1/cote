5efc01e14926d3a622040df22c63bfeeca69da5d
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-21 10:27:54 +0900

    add:배열에서 문자열 대소문자 변환하기

--- cote.js
+++ cote.js
@@ -1,13 +1,15 @@
 /*
 
 문제 설명
-알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+문자열 배열 strArr가 주어집니다. 모든 원소가 알파벳으로만 이루어져 있을 때, 배열에서 홀수번째 인덱스의 문자열은 모든 문자를 대문자로, 짝수번째 인덱스의 문자열은 모든 문자를 소문자로 바꿔서 반환하는 solution 함수를 완성해 주세요.
 
 제한사항
-1 ≤ myString의 길이 ≤ 100,000
-myString은 알파벳으로 이루어진 문자열입니다.
+1 ≤ strArr ≤ 20
+1 ≤ strArr의 원소의 길이 ≤ 20
+strArr의 원소는 알파벳으로 이루어진 문자열 입니다.
 
 
 */
 
-const solution = (myString) => myString.toLowerCase();
+const solution = (strArr) =>
+  strArr.map((arr, i) => (i % 2 === 0 ? arr.toLowerCase() : arr.toUpperCase()));
