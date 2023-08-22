f1c2886ef8f2264a40358fbb28f4ebad939015d5
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-22 14:36:03 +0900

    add:세 개의 구분자

--- cote.js
+++ cote.js
@@ -1,13 +1,22 @@
 /*
 
 문제 설명
-'m'과 "rn"이 모양이 비슷하게 생긴 점을 활용해 문자열에 장난을 하려고 합니다. 문자열 rny_string이 주어질 때, rny_string의 모든 'm'을 "rn"으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+임의의 문자열이 주어졌을 때 문자 "a", "b", "c"를 구분자로 사용해 문자열을 나누고자 합니다.
+
+예를 들어 주어진 문자열이 "baconlettucetomato"라면 나눠진 문자열 목록은 ["onlettu", "etom", "to"] 가 됩니다.
+
+문자열 myStr이 주어졌을 때 위 예시와 같이 "a", "b", "c"를 사용해 나눠진 문자열을 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+
+단, 두 구분자 사이에 다른 문자가 없을 경우에는 아무것도 저장하지 않으며, return할 배열이 빈 배열이라면 ["EMPTY"]를 return 합니다.
 
 제한사항
-1 ≤ rny_string의 길이 ≤ 100
-rny_string은 영소문자로만 이루어져 있습니다.
+1 ≤ myStr의 길이 ≤ 1,000,000
+myStr은 알파벳 소문자로 이루어진 문자열 입니다.
 
 
 */
 
-const solution = (rny_string) => rny_string.replaceAll("m", "rn");
+function solution(myStr) {
+  let answer = myStr.split(/[a|b|c]/g).filter((a) => a);
+  return answer.length ? answer : ["EMPTY"];
+}
