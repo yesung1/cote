99c21e22a606bd4a96806c6660d60ed35db20be5
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-22 17:16:58 +0900

    add:배열 만들기 6

--- cote.js
+++ cote.js
@@ -2,26 +2,30 @@
 
 
 문제 설명
-아무 원소도 들어있지 않은 빈 배열 X가 있습니다. 길이가 같은 정수 배열 arr과 boolean 배열 flag가 매개변수로 주어질 때, flag를 차례대로 순회하며 flag[i]가 true라면 X의 뒤에 arr[i]를 arr[i] × 2 번 추가하고, flag[i]가 false라면 X에서 마지막 arr[i]개의 원소를 제거한 뒤 X를 return 하는 solution 함수를 작성해 주세요.
+0과 1로만 이루어진 정수 배열 arr가 주어집니다. arr를 이용해 새로운 배열 stk을 만드려고 합니다.
+
+i의 초기값을 0으로 설정하고 i가 arr의 길이보다 작으면 다음을 반복합니다.
+
+만약 stk이 빈 배열이라면 arr[i]를 stk에 추가하고 i에 1을 더합니다.
+stk에 원소가 있고, stk의 마지막 원소가 arr[i]와 같으면 stk의 마지막 원소를 stk에서 제거하고 i에 1을 더합니다.
+stk에 원소가 있는데 stk의 마지막 원소가 arr[i]와 다르면 stk의 맨 마지막에 arr[i]를 추가하고 i에 1을 더합니다.
+위 작업을 마친 후 만들어진 stk을 return 하는 solution 함수를 완성해 주세요.
+
+단, 만약 빈 배열을 return 해야한다면 [-1]을 return 합니다.
 
 제한사항
-1 ≤ arr의 길이 = flag의 길이 ≤ 100
-arr의 모든 원소는 1 이상 9 이하의 정수입니다.
-현재 X의 길이보다 더 많은 원소를 빼는 입력은 주어지지 않습니다.
+1 ≤ arr의 길이 ≤ 1,000,000
+arr의 원소는 0 또는 1 입니다.
 
 
 
 
 */
 
-function solution(arr, flag) {
+function solution(arr) {
   var answer = [];
-  let v = 0;
-  flag.map((f, i) => {
-    f ? (v = arr[i] * 2) : (v = arr[i]);
-    for (let x = 0; x < v; x++) {
-      f ? answer.push(arr[i]) : answer.pop();
-    }
+  arr.map((n, i) => {
+    answer[answer.length - 1] === arr[i] ? answer.pop() : answer.push(arr[i]);
   });
-  return answer;
+  return answer.length ? answer : [-1];
 }
