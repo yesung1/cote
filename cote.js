e697eb15d8edc26e70052c7e3288701e1c9692cb
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-22 14:45:56 +0900

    add:배열의 원소만큼 추가하기

--- cote.js
+++ cote.js
@@ -1,22 +1,24 @@
 /*
 
+
 문제 설명
-임의의 문자열이 주어졌을 때 문자 "a", "b", "c"를 구분자로 사용해 문자열을 나누고자 합니다.
-
-예를 들어 주어진 문자열이 "baconlettucetomato"라면 나눠진 문자열 목록은 ["onlettu", "etom", "to"] 가 됩니다.
-
-문자열 myStr이 주어졌을 때 위 예시와 같이 "a", "b", "c"를 사용해 나눠진 문자열을 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
-
-단, 두 구분자 사이에 다른 문자가 없을 경우에는 아무것도 저장하지 않으며, return할 배열이 빈 배열이라면 ["EMPTY"]를 return 합니다.
+아무 원소도 들어있지 않은 빈 배열 X가 있습니다. 양의 정수 배열 arr가 매개변수로 주어질 때, arr의 앞에서부터 차례대로 원소를 보면서 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ myStr의 길이 ≤ 1,000,000
-myStr은 알파벳 소문자로 이루어진 문자열 입니다.
+1 ≤ arr의 길이 ≤ 100
+1 ≤ arr의 원소 ≤ 100
+
 
 
 */
 
-function solution(myStr) {
-  let answer = myStr.split(/[a|b|c]/g).filter((a) => a);
-  return answer.length ? answer : ["EMPTY"];
+function solution(arr) {
+  var answer = [];
+  let v = 0;
+  arr.map((n, i) => {
+    for (let x = 0; x < arr[i]; x++) {
+      answer.push(arr[i]);
+    }
+  });
+  return answer;
 }
