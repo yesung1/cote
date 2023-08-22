f4a3de67903f79f9e4b35f9135aad7edfb4eee3d
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-22 10:54:17 +0900

    add:문자열 잘라서 정렬하기

--- cote.js
+++ cote.js
@@ -1,14 +1,19 @@
 /*
 
 문제 설명
-문자열 myString이 주어집니다. myString을 문자 "x"를 기준으로 나눴을 때 나눠진 문자열 각각의 길이를 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+문자열 myString이 주어집니다. "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
+
+단, 빈 문자열은 반환할 배열에 넣지 않습니다.
 
 제한사항
-1 ≤ myString의 길이 ≤ 100,000
+1 ≤ myString ≤ 100,000
 myString은 알파벳 소문자로 이루어진 문자열입니다.
-
 
 
 */
 
-const solution = (myString) => myString.split("x").map((str) => str.length);
+const solution = (myString) =>
+  myString
+    .split("x")
+    .filter((n) => n)
+    .sort();
