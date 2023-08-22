890f514dd1546bdeb253e96150993567d4dd0536
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-22 11:41:31 +0900

    add:rny_string

--- cote.js
+++ cote.js
@@ -1,22 +1,13 @@
 /*
 
 문제 설명
-문자 "A"와 "B"로 이루어진 문자열 myString과 pat가 주어집니다. myString의 "A"를 "B"로, "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중 pat이 있으면 1을 아니면 0을 return 하는 solution 함수를 완성하세요.
+'m'과 "rn"이 모양이 비슷하게 생긴 점을 활용해 문자열에 장난을 하려고 합니다. 문자열 rny_string이 주어질 때, rny_string의 모든 'm'을 "rn"으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ myString의 길이 ≤ 100
-1 ≤ pat의 길이 ≤ 10
-myString과 pat는 문자 "A"와 "B"로만 이루어진 문자열입니다.
+1 ≤ rny_string의 길이 ≤ 100
+rny_string은 영소문자로만 이루어져 있습니다.
 
 
 */
 
-function solution(myString, pat) {
-  var answer = "";
-  [...myString].map((item, i) => {
-    item === "A" ? (answer += "B") : (answer += "A");
-  });
-  console.log(answer);
-  answer = answer.includes(pat) === true ? 1 : 0;
-  return answer;
-}
+const solution = (rny_string) => rny_string.replaceAll("m", "rn");
