f3ebd1efa550532a4b8f8a6b3ede720bc4bfcc4f
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-22 11:39:22 +0900

    add:간단한 식 계산하기

--- cote.js
+++ cote.js
@@ -1,19 +1,31 @@
 /*
 
 문제 설명
-문자열 myString이 주어집니다. "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
-
-단, 빈 문자열은 반환할 배열에 넣지 않습니다.
+문자열 binomial이 매개변수로 주어집니다. binomial은 "a op b" 형태의 이항식이고 a와 b는 음이 아닌 정수, op는 '+', '-', '*' 중 하나입니다. 주어진 식을 계산한 정수를 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ myString ≤ 100,000
-myString은 알파벳 소문자로 이루어진 문자열입니다.
+0 ≤ a, b ≤ 40,000
+0을 제외하고 a, b는 0으로 시작하지 않습니다.
 
 
 */
 
-const solution = (myString) =>
-  myString
-    .split("x")
-    .filter((n) => n)
-    .sort();
+function solution(binomial) {
+  var answer = 0;
+  let s = binomial.split(" ");
+  let n1 = Number(s[0]);
+  let op = s[1];
+  let n2 = Number(s[2]);
+  switch (op) {
+    case "-":
+      answer = n1 - n2;
+      break;
+    case "+":
+      answer = n1 + n2;
+      break;
+    case "*":
+      answer = n1 * n2;
+      break;
+  }
+  return answer;
+}
