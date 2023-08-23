f0164672ba37e1ff69ad99871715daf42bd4e8dc
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-23 14:55:50 +0900

    add:문자열 정수의 합

--- cote.js
+++ cote.js
@@ -2,12 +2,13 @@
 
 
 문제 설명
-실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+한 자리 정수로 이루어진 문자열 num_str이 주어질 때, 각 자리수의 합을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 ≤ flo ≤ 100
+3 ≤ num_str ≤ 100
 
 
 */
 
-const solution = (flo) => Math.floor(flo);
+const solution = (num_str) =>
+  [...num_str].reduce((acc, cur) => (acc += Number(cur)), 0);
