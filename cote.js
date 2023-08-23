fea89311de1eb690a8ff47aeaa40f1fdf1b4eb5d
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-23 14:54:46 +0900

    add:전국 대회 선발 고사

--- cote.js
+++ cote.js
@@ -2,14 +2,31 @@
 
 
 문제 설명
-정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 제외한 수들을 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+0번부터 n - 1번까지 n명의 학생 중 3명을 선발하는 전국 대회 선발 고사를 보았습니다. 등수가 높은 3명을 선발해야 하지만, 개인 사정으로 전국 대회에 참여하지 못하는 학생들이 있어 참여가 가능한 학생 중 등수가 높은 3명을 선발하기로 했습니다.
+
+각 학생들의 선발 고사 등수를 담은 정수 배열 rank와 전국 대회 참여 가능 여부가 담긴 boolean 배열 attendance가 매개변수로 주어집니다. 전국 대회에 선발된 학생 번호들을 등수가 높은 순서대로 각각 a, b, c번이라고 할 때 10000 × a + 100 × b + c를 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-6 ≤ num_list의 길이 ≤ 30
-1 ≤ num_list의 원소 ≤ 100
-
+3 ≤ rank의 길이 = attendance의 길이 ≤ 100
+rank[i]는 i번 학생의 선발 고사 등수를 의미합니다.
+rank의 원소는 1부터 n까지의 정수로 모두 서로 다릅니다.
+attendance[i]는 i번 학생의 전국 대회 참석 가능 여부를 나타냅니다.
+attendance[i]가 true라면 참석 가능, false면 참석 불가능을 의미합니다.
+attendance의 원소 중 적어도 3개는 true입니다.
 
 
 */
 
-const solution = (num_list) => num_list.sort((a, b) => a - b).slice(5);
+function solution(rank, attendance) {
+  var answer = 0;
+  let arr = [];
+  let arr2 = [];
+  attendance.filter((a, i) => {
+    if (a) return arr.push(rank[i]);
+  });
+  arr = arr.sort((a, b) => a - b).slice(0, 3);
+  arr2 = arr.map((a, i) => {
+    return rank.indexOf(a);
+  });
+  return 10000 * arr2[0] + 100 * arr2[1] + arr2[2];
+}
