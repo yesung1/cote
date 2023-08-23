e00a352288dc8da0642d27200ed0bd3a054da740
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-23 14:05:32 +0900

    add:문자열 묶기

--- cote.js
+++ cote.js
@@ -2,30 +2,27 @@
 
 
 문제 설명
-이 문제에서 두 정수 배열의 대소관계를 다음과 같이 정의합니다.
-
-두 배열의 길이가 다르다면, 배열의 길이가 긴 쪽이 더 큽니다.
-배열의 길이가 같다면 각 배열에 있는 모든 원소의 합을 비교하여 다르다면 더 큰 쪽이 크고, 같다면 같습니다.
-두 정수 배열 arr1과 arr2가 주어질 때, 위에서 정의한 배열의 대소관계에 대하여 arr2가 크다면 -1, arr1이 크다면 1, 두 배열이 같다면 0을 return 하는 solution 함수를 작성해 주세요.
+문자열 배열 strArr이 주어집니다. strArr의 원소들을 길이가 같은 문자열들끼리 그룹으로 묶었을 때 가장 개수가 많은 그룹의 크기를 return 하는 solution 함수를 완성해 주세요.
 
 제한사항
-1 ≤ arr1의 길이 ≤ 100
-1 ≤ arr2의 길이 ≤ 100
-1 ≤ arr1의 원소 ≤ 100
-1 ≤ arr2의 원소 ≤ 100
-문제에서 정의한 배열의 대소관계가 일반적인 프로그래밍 언어에서 정의된 배열의 대소관계와 다를 수 있는 점에 유의해주세요.
+1 ≤ strArr의 길이 ≤ 100,000
+1 ≤ strArr의 원소의 길이 ≤ 30
+strArr의 원소들은 알파벳 소문자로 이루어진 문자열입니다.
+
 
 
 
 
 */
 
-function solution(arr1, arr2) {
-  let leng1 = arr1.length;
-  let leng2 = arr2.length;
-  let v1 = arr1.reduce((acc, curr) => acc + curr);
-  let v2 = arr2.reduce((acc, curr) => acc + curr);
-  if (leng1 !== leng2) return leng1 > leng2 ? 1 : -1;
-  if (v1 === v2) return 0;
-  return v1 > v2 ? 1 : -1;
+function solution(strArr) {
+  var answer = 0;
+  let counts = {};
+  let leng1 = 0;
+  strArr.map((s, i) => {
+    leng1 = s.length;
+    counts[leng1] ? (counts[leng1] += 1) : (counts[leng1] = 1);
+  });
+  answer = Math.max(...Object.values(counts));
+  return answer;
 }
