cd70e2e441137651bfd4758fb2cb5646821f4191
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-23 14:10:12 +0900

    add:뒤에서 5등 위로

--- cote.js
+++ cote.js
@@ -2,7 +2,7 @@
 
 
 문제 설명
-정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 제외한 수들을 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
 6 ≤ num_list의 길이 ≤ 30
@@ -10,12 +10,6 @@
 
 
 
-
-
 */
 
-const solution = (num_list) =>
-  num_list
-    .sort((a, b) => b - a)
-    .splice(-5)
-    .reverse();
+const solution = (num_list) => num_list.sort((a, b) => a - b).slice(5);
