f61fa0debf1a20baead9588588717afc48981202
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-23 14:56:14 +0900

    add:문자열을 정수로 변환하기

--- cote.js
+++ cote.js
@@ -1,14 +1,13 @@
 /*
 
-
 문제 설명
-한 자리 정수로 이루어진 문자열 num_str이 주어질 때, 각 자리수의 합을 return하도록 solution 함수를 완성해주세요.
+숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-3 ≤ num_str ≤ 100
+1 ≤ n_str ≤ 5
+n_str은 0부터 9까지의 정수 문자로만 이루어져 있습니다.
 
 
 */
 
-const solution = (num_str) =>
-  [...num_str].reduce((acc, cur) => (acc += Number(cur)), 0);
+const solution = (n_str) => Number(n_str);
