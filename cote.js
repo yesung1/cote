392110ffeb92346a1d4d593f3603cf71d2564f58
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-24 15:51:21 +0900

    add:주사위 게임 1

--- cote.js
+++ cote.js
@@ -1,13 +1,24 @@
 /*
 
 문제 설명
-정수 리스트 num_list와 찾으려는 정수 n이 주어질 때, num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
+1부터 6까지 숫자가 적힌 주사위가 두 개 있습니다. 두 주사위를 굴렸을 때 나온 숫자를 각각 a, b라고 했을 때 얻는 점수는 다음과 같습니다.
+
+a와 b가 모두 홀수라면 a2 + b2 점을 얻습니다.
+a와 b 중 하나만 홀수라면 2 × (a + b) 점을 얻습니다.
+a와 b 모두 홀수가 아니라면 |a - b| 점을 얻습니다.
+두 정수 a와 b가 매개변수로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-3 ≤ num_list의 길이 ≤ 100
-1 ≤ num_list의 원소 ≤ 100
-1 ≤ n ≤ 100
+a와 b는 1 이상 6 이하의 정수입니다.
+
 
 */
 
-const solution = (num_list, n) => (num_list.includes(n) ? 1 : 0);
+function solution(a, b) {
+  let n1 = a % 2;
+  let n2 = b % 2;
+  let answer = 0;
+  if (n1 && n2) return (answer = a ** 2 + b ** 2);
+  if (n1 || n2) return (answer = 2 * (a + b));
+  return (answer = Math.abs(a - b));
+}
