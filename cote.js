723e6c83087f6fb045c014a4a22a39920093a783
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-24 15:50:11 +0900

    add:부분 문자열

--- cote.js
+++ cote.js
@@ -1,16 +1,14 @@
 /*
 
 문제 설명
-부분 문자열이란 문자열에서 연속된 일부분에 해당하는 문자열을 의미합니다. 예를 들어, 문자열 "ana", "ban", "anana", "banana", "n"는 모두 문자열 "banana"의 부분 문자열이지만, "aaa", "bnana", "wxyz"는 모두 "banana"의 부분 문자열이 아닙니다.
+어떤 문자열 A가 다른 문자열 B안에 속하면 A를 B의 부분 문자열이라고 합니다. 예를 들어 문자열 "abc"는 문자열 "aabcc"의 부분 문자열입니다.
 
-문자열 my_string과 target이 매개변수로 주어질 때, target이 문자열 my_string의 부분 문자열이라면 1을, 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
+문자열 str1과 str2가 주어질 때, str1이 str2의 부분 문자열이라면 1을 부분 문자열이 아니라면 0을 return하도록 solution 함수를 완성해주세요.
 
-제한사항
-1 ≤ my_string의 길이 ≤ 100
-my_string은 영소문자로만 이루어져 있습니다.
-1 ≤ target의 길이 ≤ 100
-target은 영소문자로만 이루어져 있습니다.
+제한 사항
+1 ≤ str1 ≤ str2 ≤ 20
+str1과 str2는 영어 소문자로만 이루어져 있습니다.
 
 */
 
-const solution = (my_string, target) => (my_string.includes(target) ? 1 : 0);
+const solution = (str1, str2) => (str2.includes(str1) ? 1 : 0);
