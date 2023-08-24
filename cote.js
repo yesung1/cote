293cfe999096668c0d4b0875ccc9a1e845ccedba
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-24 15:49:49 +0900

    add:부분 문자열인지 확인하기

--- cote.js
+++ cote.js
@@ -1,18 +1,16 @@
 /*
 
 문제 설명
-정수 배열 arr과 delete_list가 있습니다. arr의 원소 중 delete_list의 원소를 모두 삭제하고 남은 원소들은 기존의 arr에 있던 순서를 유지한 배열을 return 하는 solution 함수를 작성해 주세요.
+부분 문자열이란 문자열에서 연속된 일부분에 해당하는 문자열을 의미합니다. 예를 들어, 문자열 "ana", "ban", "anana", "banana", "n"는 모두 문자열 "banana"의 부분 문자열이지만, "aaa", "bnana", "wxyz"는 모두 "banana"의 부분 문자열이 아닙니다.
+
+문자열 my_string과 target이 매개변수로 주어질 때, target이 문자열 my_string의 부분 문자열이라면 1을, 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
-1 ≤ arr의 길이 ≤ 100
-1 ≤ arr의 원소 ≤ 1,000
-arr의 원소는 모두 서로 다릅니다.
-1 ≤ delete_list의 길이 ≤ 100
-1 ≤ delete_list의 원소 ≤ 1,000
-delete_list의 원소는 모두 서로 다릅니다.
-
+1 ≤ my_string의 길이 ≤ 100
+my_string은 영소문자로만 이루어져 있습니다.
+1 ≤ target의 길이 ≤ 100
+target은 영소문자로만 이루어져 있습니다.
 
 */
 
-const solution = (arr, delete_list) =>
-  arr.filter((n) => !delete_list.includes(n));
+const solution = (my_string, target) => (my_string.includes(target) ? 1 : 0);
