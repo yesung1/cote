97406b706132ec01a7ed443ab0a80641fef6024c
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-24 15:23:54 +0900

    Revert "add:이차원 배열 대각선 순회하기"
    This reverts commit a7127acb6788b058314fb15b62f58256408c03ea.

--- cote.js
+++ cote.js
@@ -1,27 +1,13 @@
 /*
 
 문제 설명
-2차원 정수 배열 board와 정수 k가 주어집니다.
-
-i + j <= k를 만족하는 모든 (i, j)에 대한 board[i][j]의 합을 return 하는 solution 함수를 완성해 주세요.
+숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ board의 길이 ≤ 100
-1 ≤ board[i]의 길이 ≤ 100
-1 ≤ board[i][j] ≤ 10,000
-모든 board[i]의 길이는 같습니다.
-0 ≤ k < board의 길이 + board[i]의 길이
+1 ≤ n_str ≤ 5
+n_str은 0부터 9까지의 정수 문자로만 이루어져 있습니다.
 
 
 */
 
-function solution(board, k) {
-  let sum = 0;
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (i + j <= k) sum += board[i][j];
-    }
-  }
-
-  return sum;
-}
+const solution = (n_str) => Number(n_str);
