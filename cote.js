7cfd3e529e489061b02277b689fda00af91dbc67
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-24 15:50:59 +0900

    add:정수 찾기

--- cote.js
+++ cote.js
@@ -1,16 +1,13 @@
 /*
 
 문제 설명
-문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열이라고 합니다. 꼬리 문자열을 만들 때 특정 문자열을 포함한 문자열은 제외시키려고 합니다. 예를 들어 문자열 리스트 ["abc", "def", "ghi"]가 있고 문자열 "ef"를 포함한 문자열은 제외하고 꼬리 문자열을 만들면 "abcghi"가 됩니다.
-
-문자열 리스트 str_list와 제외하려는 문자열 ex가 주어질 때, str_list에서 ex를 포함한 문자열을 제외하고 만든 꼬리 문자열을 return하도록 solution 함수를 완성해주세요.
+정수 리스트 num_list와 찾으려는 정수 n이 주어질 때, num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-2 ≤ str_list의 길이 ≤ 10
-1 ≤ str_list의 원소의 길이 ≤ 10
-1 ≤ ex의 길이 ≤ 5
+3 ≤ num_list의 길이 ≤ 100
+1 ≤ num_list의 원소 ≤ 100
+1 ≤ n ≤ 100
 
 */
 
-const solution = (str_list, ex) =>
-  str_list.filter((str) => !str.includes(ex)).join("");
+const solution = (num_list, n) => (num_list.includes(n) ? 1 : 0);
