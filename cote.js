82924d6b0e86c98446a9ee1a5b79ac7bc79577c8
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-24 15:48:08 +0900

    add:문자열로 변환

--- cote.js
+++ cote.js
@@ -1,15 +1,12 @@
 /*
 
 문제 설명
-0 이상의 두 정수가 문자열 a, b로 주어질 때, a + b의 값을 문자열로 return 하는 solution 함수를 작성해 주세요.
+정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ a의 길이 ≤ 100,000
-1 ≤ b의 길이 ≤ 100,000
-a와 b는 숫자로만 이루어져 있습니다.
-a와 b는 정수 0이 아니라면 0으로 시작하지 않습니다.
+1 ≤ n ≤ 10000
 
 
 */
 
-const solution = (a, b) => String(BigInt(a) + BigInt(b));
+const solution = String;
