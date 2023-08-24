f682a79ce84614d49c816a9ac2fb5c7f924cb4ea
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-24 15:27:44 +0900

    add:0 떼기

--- cote.js
+++ cote.js
@@ -1,13 +1,13 @@
 /*
 
 문제 설명
-숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ n_str ≤ 5
-n_str은 0부터 9까지의 정수 문자로만 이루어져 있습니다.
+2 ≤ n_str ≤ 10
+n_str이 "0"으로만 이루어진 경우는 없습니다.
 
 
 */
 
-const solution = (n_str) => Number(n_str);
+const solution = (n_str) => String(Number(n_str));
