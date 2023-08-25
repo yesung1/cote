c957e4b18fdf6a03079ca7663aa350cdd602f3cb
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-25 09:40:09 +0900

    add:커피 심부름

--- cote.js
+++ cote.js
@@ -1,17 +1,29 @@
 /*
 
 문제 설명
-정수 배열 date1과 date2가 주어집니다. 두 배열은 각각 날짜를 나타내며 [year, month, day] 꼴로 주어집니다. 각 배열에서 year는 연도를, month는 월을, day는 날짜를 나타냅니다.
+팀의 막내인 철수는 아메리카노와 카페 라테만 판매하는 카페에서 팀원들의 커피를 사려고 합니다. 아메리카노와 카페 라테의 가격은 차가운 것과 뜨거운 것 상관없이 각각 4500, 5000원입니다. 각 팀원에게 마실 메뉴를 적어달라고 하였고, 그 중에서 메뉴만 적은 팀원의 것은 차가운 것으로 통일하고 "아무거나"를 적은 팀원의 것은 차가운 아메리카노로 통일하기로 하였습니다.
 
-만약 date1이 date2보다 앞서는 날짜라면 1을, 아니면 0을 return 하는 solution 함수를 완성해 주세요.
+각 직원이 적은 메뉴가 문자열 배열 order로 주어질 때, 카페에서 결제하게 될 금액을 return 하는 solution 함수를 작성해주세요. order의 원소는 아래의 것들만 들어오고, 각각의 의미는 다음과 같습니다.
 
+order의 원소	의미
+"iceamericano", "americanoice"	차가운 아메리카노
+"hotamericano", "americanohot"	따뜻한 아메리카노
+"icecafelatte", "cafelatteice"	차가운 카페 라테
+"hotcafelatte", "cafelattehot"	따뜻한 카페 라테
+"americano"	아메리카노
+"cafelatte"	카페 라테
+"anything"	아무거나
 제한사항
-date1의 길이 = date2의 길이 = 3
-0 ≤ year ≤ 10,000
-1 ≤ month ≤ 12
-day는 month에 따라 가능한 날짜로 주어집니다.
+1 ≤ order의 길이 ≤ 1,000
 
 
 */
 
-const solution = (date1, date2) => (new Date(date1) < new Date(date2) ? 1 : 0);
+const solution = (order) =>
+  order.reduce(
+    (acc, curr) =>
+      curr.includes("americano") || curr.includes("anything")
+        ? (acc += 4500)
+        : (acc += 5000),
+    0
+  );
