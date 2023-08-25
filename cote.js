02b883dc564693cf3567ff4c232531a48535a5d8
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-25 11:41:33 +0900

    add:그림 확대

--- cote.js
+++ cote.js
@@ -1,29 +1,22 @@
 /*
 
 문제 설명
-팀의 막내인 철수는 아메리카노와 카페 라테만 판매하는 카페에서 팀원들의 커피를 사려고 합니다. 아메리카노와 카페 라테의 가격은 차가운 것과 뜨거운 것 상관없이 각각 4500, 5000원입니다. 각 팀원에게 마실 메뉴를 적어달라고 하였고, 그 중에서 메뉴만 적은 팀원의 것은 차가운 것으로 통일하고 "아무거나"를 적은 팀원의 것은 차가운 아메리카노로 통일하기로 하였습니다.
+직사각형 형태의 그림 파일이 있고, 이 그림 파일은 1 × 1 크기의 정사각형 크기의 픽셀로 이루어져 있습니다. 이 그림 파일을 나타낸 문자열 배열 picture과 정수 k가 매개변수로 주어질 때, 이 그림 파일을 가로 세로로 k배 늘린 그림 파일을 나타내도록 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
 
-각 직원이 적은 메뉴가 문자열 배열 order로 주어질 때, 카페에서 결제하게 될 금액을 return 하는 solution 함수를 작성해주세요. order의 원소는 아래의 것들만 들어오고, 각각의 의미는 다음과 같습니다.
-
-order의 원소	의미
-"iceamericano", "americanoice"	차가운 아메리카노
-"hotamericano", "americanohot"	따뜻한 아메리카노
-"icecafelatte", "cafelatteice"	차가운 카페 라테
-"hotcafelatte", "cafelattehot"	따뜻한 카페 라테
-"americano"	아메리카노
-"cafelatte"	카페 라테
-"anything"	아무거나
 제한사항
-1 ≤ order의 길이 ≤ 1,000
+1 ≤ picture의 길이 ≤ 20
+1 ≤ picture의 원소의 길이 ≤ 20
+모든 picture의 원소의 길이는 같습니다.
+picture의 원소는 '.'과 'x'로 이루어져 있습니다.
+1 ≤ k ≤ 10
 
 
 */
 
-const solution = (order) =>
-  order.reduce(
-    (acc, curr) =>
-      curr.includes("americano") || curr.includes("anything")
-        ? (acc += 4500)
-        : (acc += 5000),
-    0
-  );
+function solution(picture, k) {
+  var answer = [];
+  for (let p of picture) {
+    answer.push(...Array(k).fill([...p].map((n) => n.repeat(k)).join("")));
+  }
+  return answer;
+}
