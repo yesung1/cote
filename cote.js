c2aeec8608e5cee3c183c217349f38ebbe780e18
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:38:08 +0900

    add:두 수의 합

--- cote.js
+++ cote.js
@@ -2,26 +2,16 @@
 
 
 문제 설명
-2차원 정수 배열 board와 정수 k가 주어집니다.
-
-i + j <= k를 만족하는 모든 (i, j)에 대한 board[i][j]의 합을 return 하는 solution 함수를 완성해 주세요.
+정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
 
 제한사항
-1 ≤ board의 길이 ≤ 100
-1 ≤ board[i]의 길이 ≤ 100
-1 ≤ board[i][j] ≤ 10,000
-모든 board[i]의 길이는 같습니다.
-0 ≤ k < board의 길이 + board[i]의 길이
+-50,000 ≤ num1 ≤ 50,000
+-50,000 ≤ num2 ≤ 50,000
+
 
 */
 
-function solution(board, k) {
-  let sum = 0;
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (i + j <= k) sum += board[i][j];
-    }
-  }
-
-  return sum;
+function solution(num1, num2) {
+  var answer = -1;
+  return (answer = num1 + num2);
 }
