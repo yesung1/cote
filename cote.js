8867f11af835c488f8aeee388f910dbf30ce4b32
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:46:24 +0900

    add:두 수의 나눗셈

--- cote.js
+++ cote.js
@@ -2,7 +2,7 @@
 
 
 문제 설명
-정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
 
 제한사항
 0 < num1 ≤ 100
@@ -11,4 +11,4 @@
 
 */
 
-const solution = (num1, num2) => Math.floor(num1 / num2);
+const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
