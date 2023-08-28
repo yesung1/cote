14d4ef9e05e521ca8b6410292f4629bf4d9af74a
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:36:46 +0900

    add:특별한 이차원 배열 2

--- cote.js
+++ cote.js
@@ -1,43 +1,23 @@
 /*
 
 문제 설명
-양의 정수 n이 매개변수로 주어집니다. n × n 배열에 1부터 n2 까지 정수를 인덱스 [0][0]부터 시계방향 나선형으로 배치한 이차원 배열을 return 하는 solution 함수를 작성해 주세요.
+n × n 크기의 이차원 배열 arr이 매개변수로 주어질 때, arr이 다음을 만족하면 1을 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
 
+0 ≤ i, j < n인 정수 i, j에 대하여 arr[i][j] = arr[j][i]
 제한사항
-1 ≤ n ≤ 30
-
+1 ≤ arr의 길이 = arr의 원소의 길이 ≤ 100
+1 ≤ arr의 원소의 원소 ≤ 1,000
+모든 arr의 원소의 길이는 같습니다.
 
 */
 
-function solution(n) {
-  const matrix = new Array(n).fill().map(() => new Array(n).fill(0));
-  let left = 0;
-  let right = n - 1;
-  let top = 0;
-  let bottom = n - 1;
-  let num = 1;
-
-  while (left <= right && top <= bottom) {
-    for (let i = left; i <= right; i++) {
-      matrix[top][i] = num;
-      num++;
+function solution(arr) {
+  var answer = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (!answer) return 0;
+      if (arr[i][j] !== arr[j][i]) answer = 0;
     }
-    top++;
-    for (let i = top; i <= bottom; i++) {
-      matrix[i][right] = num;
-      num++;
-    }
-    right--;
-    for (let i = right; i >= left; i--) {
-      matrix[bottom][i] = num;
-      num++;
-    }
-    bottom--;
-    for (let i = bottom; i >= top; i--) {
-      matrix[i][left] = num;
-      num++;
-    }
-    left++;
   }
-  return matrix;
+  return answer;
 }
