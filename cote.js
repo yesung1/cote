061f8dc41334ab306d0a3774411893b389b9c559
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:37:07 +0900

    add:정사각형으로 만들기

--- cote.js
+++ cote.js
@@ -1,23 +1,26 @@
 /*
 
-문제 설명
-n × n 크기의 이차원 배열 arr이 매개변수로 주어질 때, arr이 다음을 만족하면 1을 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
 
-0 ≤ i, j < n인 정수 i, j에 대하여 arr[i][j] = arr[j][i]
+문제 설명
+이차원 정수 배열 arr이 매개변수로 주어집니다. arr의 행의 수가 더 많다면 열의 수가 행의 수와 같아지도록 각 행의 끝에 0을 추가하고, 열의 수가 더 많다면 행의 수가 열의 수와 같아지도록 각 열의 끝에 0을 추가한 이차원 배열을 return 하는 solution 함수를 작성해 주세요.
+
 제한사항
-1 ≤ arr의 길이 = arr의 원소의 길이 ≤ 100
+1 ≤ arr의 길이 ≤ 100
+1 ≤ arr의 원소의 길이 ≤ 100
+arr의 모든 원소의 길이는 같습니다.
 1 ≤ arr의 원소의 원소 ≤ 1,000
-모든 arr의 원소의 길이는 같습니다.
 
 */
 
 function solution(arr) {
-  var answer = 1;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (!answer) return 0;
-      if (arr[i][j] !== arr[j][i]) answer = 0;
-    }
-  }
-  return answer;
+  var answer = [];
+  let arr1 = arr.length;
+  let arr2 = arr[0].length;
+  if (arr1 === arr2) return (answer = arr);
+  arr1 > arr2
+    ? (arr = arr.map((a) => [...a, ...new Array(arr1 - arr2).fill(0)]))
+    : (arr = arr.concat(
+        Array.from(Array(arr2 - arr1), () => Array(arr2).fill(0))
+      ));
+  return (answer = arr);
 }
