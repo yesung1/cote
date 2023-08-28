a30432398aab5815334b9c1dbf69e2e0f4efb391
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:38:55 +0900

    add:두 수의 차

--- cote.js
+++ cote.js
@@ -2,16 +2,13 @@
 
 
 문제 설명
-정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
 
 제한사항
--50,000 ≤ num1 ≤ 50,000
--50,000 ≤ num2 ≤ 50,000
+-50000 ≤ num1 ≤ 50000
+-50000 ≤ num2 ≤ 50000
 
 
 */
 
-function solution(num1, num2) {
-  var answer = -1;
-  return (answer = num1 + num2);
-}
+const solution = (num1, num2) => num1 - num2;
