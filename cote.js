203626a92124da44e8f7f83cbc15a839d333074b
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:49:44 +0900

    add:숫자 비교하기

--- cote.js
+++ cote.js
@@ -2,13 +2,13 @@
 
 
 문제 설명
-정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 < num1 ≤ 100
-0 < num2 ≤ 100
+0 ≤ num1 ≤ 10,000
+0 ≤ num2 ≤ 10,000
 
 
 */
 
-const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
+const solution = (num1, num2) => (num1 === num2 ? 1 : -1);
