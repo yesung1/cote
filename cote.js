2a7472f26786735eba1bd347e6e5af7db17c37c4
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:40:47 +0900

    add:두 수의 곱

--- cote.js
+++ cote.js
@@ -2,13 +2,14 @@
 
 
 문제 설명
-정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
 
 제한사항
--50000 ≤ num1 ≤ 50000
--50000 ≤ num2 ≤ 50000
+0 ≤ num1 ≤ 100
+0 ≤ num2 ≤ 100
+
 
 
 */
 
-const solution = (num1, num2) => num1 - num2;
+const solution = (num1, num2) => num1 * num2;
