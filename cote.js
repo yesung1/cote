1642c536b2b6d4b0727efd50d42ebae9dbbc5df3
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-28 11:43:23 +0900

    add:몫 구하기

--- cote.js
+++ cote.js
@@ -2,14 +2,13 @@
 
 
 문제 설명
-정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 ≤ num1 ≤ 100
-0 ≤ num2 ≤ 100
-
+0 < num1 ≤ 100
+0 < num2 ≤ 100
 
 
 */
 
-const solution = (num1, num2) => num1 * num2;
+const solution = (num1, num2) => Math.floor(num1 / num2);
