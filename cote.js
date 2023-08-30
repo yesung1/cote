f57ef6bf5ddeabafa662948fe64ae410f181193b
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 11:42:31 +0900

    add:피자 나눠 먹기 (1)

--- cote.js
+++ cote.js
@@ -2,15 +2,11 @@
 
 
 문제 설명
-정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
 
 제한사항
 1 ≤ n ≤ 100
 
 */
 
-function solution(n) {
-  var answer = [];
-  for (let x = 1; x <= n; x += 2) answer.push(x);
-  return answer;
-}
+const solution = (n) => Math.ceil(n / 7);
