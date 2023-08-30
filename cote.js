c00e519ab880b9dfcae2103a9bfd6fb03404f440
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 12:04:48 +0900

    add:배열의 평균값

--- cote.js
+++ cote.js
@@ -2,12 +2,14 @@
 
 
 문제 설명
-머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-2 ≤ slice ≤ 10
-1 ≤ n ≤ 100
+0 ≤ numbers의 원소 ≤ 1,000
+1 ≤ numbers의 길이 ≤ 100
+정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
 
 */
 
-const solution = (slice, n) => Math.ceil(n / slice);
+const solution = (numbers) =>
+  numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
