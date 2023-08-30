07d5070cc96a02a22e24ee729b67897f5c4e56f4
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 10:16:53 +0900

    add:중앙값 구하기

--- cote.js
+++ cote.js
@@ -2,13 +2,14 @@
 
 
 문제 설명
-정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-0 < num1 ≤ 100
-0 < num2 ≤ 100
-
+array의 길이는 홀수입니다.
+0 < array의 길이 < 100
+-1,000 < array의 원소 < 1,000
 
 */
 
-const solution = (num1, num2) => num1 % num2;
+const solution = (array) =>
+  array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
