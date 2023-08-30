05e5166c8eca15b9826370e5c3e440d3a7865a23
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 09:51:02 +0900

    add:나머지 구하기

--- cote.js
+++ cote.js
@@ -2,12 +2,13 @@
 
 
 문제 설명
-정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
 
 제한사항
--10,000 ≤ numbers의 원소 ≤ 10,000
-1 ≤ numbers의 길이 ≤ 1,000
+0 < num1 ≤ 100
+0 < num2 ≤ 100
+
 
 */
 
-const solution = (numbers) => numbers.map((n) => n * 2);
+const solution = (num1, num2) => num1 % num2;
