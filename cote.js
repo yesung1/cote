9e80448d7313ecfee2fb078511cae4b8658097a4
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 11:23:36 +0900

    add:최빈값 구하기

--- cote.js
+++ cote.js
@@ -2,14 +2,20 @@
 
 
 문제 설명
-중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
+최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
 
 제한사항
-array의 길이는 홀수입니다.
 0 < array의 길이 < 100
--1,000 < array의 원소 < 1,000
+0 ≤ array의 원소 < 1000
 
 */
 
-const solution = (array) =>
-  array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
+function solution(array) {
+  const counts = array.reduce(
+    (acc, num) => ((acc[num] = (acc[num] || 0) + 1), acc),
+    {}
+  );
+  const maxCount = Math.max(...Object.values(counts));
+  let answer = Object.keys(counts).filter((num) => counts[num] === maxCount);
+  return answer.length === 1 ? Number(answer) : -1;
+}
