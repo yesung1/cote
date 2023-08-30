5cd2baa2cdf1c09881ca146cbf0f11a08b072d83
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 09:47:30 +0900

    add:분수의 덧셈

--- cote.js
+++ cote.js
@@ -2,13 +2,21 @@
 
 
 문제 설명
-정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다. 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-0 ≤ num1 ≤ 10,000
-0 ≤ num2 ≤ 10,000
-
+0 <numer1, denom1, numer2, denom2 < 1,000
 
 */
 
-const solution = (num1, num2) => (num1 === num2 ? 1 : -1);
+function cal_gcd(a, b) {
+  return a % b === 0 ? b : cal_gcd(b, a % b);
+}
+
+function solution(denum1, num1, denum2, num2) {
+  let denum = denum1 * num2 + denum2 * num1;
+  let num = num1 * num2;
+  console.log(denum, num);
+  let gcd = cal_gcd(denum, num);
+  return [denum / gcd, num / gcd];
+}
