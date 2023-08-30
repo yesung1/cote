28180d5d388638678665c1b7805c72e8f1aca3da
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 09:49:47 +0900

    add:배열 두배 만들기

--- cote.js
+++ cote.js
@@ -2,21 +2,12 @@
 
 
 문제 설명
-첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다. 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 <numer1, denom1, numer2, denom2 < 1,000
+-10,000 ≤ numbers의 원소 ≤ 10,000
+1 ≤ numbers의 길이 ≤ 1,000
 
 */
 
-function cal_gcd(a, b) {
-  return a % b === 0 ? b : cal_gcd(b, a % b);
-}
-
-function solution(denum1, num1, denum2, num2) {
-  let denum = denum1 * num2 + denum2 * num1;
-  let num = num1 * num2;
-  console.log(denum, num);
-  let gcd = cal_gcd(denum, num);
-  return [denum / gcd, num / gcd];
-}
+const solution = (numbers) => numbers.map((n) => n * 2);
