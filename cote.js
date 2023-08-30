c4c3ed830a7785280b17d828fe1de0e9da31741a
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-30 11:29:59 +0900

    add:짝수는 싫어요

--- cote.js
+++ cote.js
@@ -2,20 +2,15 @@
 
 
 문제 설명
-최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
+정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 < array의 길이 < 100
-0 ≤ array의 원소 < 1000
+1 ≤ n ≤ 100
 
 */
 
-function solution(array) {
-  const counts = array.reduce(
-    (acc, num) => ((acc[num] = (acc[num] || 0) + 1), acc),
-    {}
-  );
-  const maxCount = Math.max(...Object.values(counts));
-  let answer = Object.keys(counts).filter((num) => counts[num] === maxCount);
-  return answer.length === 1 ? Number(answer) : -1;
+function solution(n) {
+  var answer = [];
+  for (let x = 1; x <= n; x += 2) answer.push(x);
+  return answer;
 }
