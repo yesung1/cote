5a280e1cb1acf5faa8f00c54837637d71fd8efcc
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-31 10:08:30 +0900

    add:문자열 뒤집기

--- cote.js
+++ cote.js
@@ -1,13 +1,13 @@
 /*
 
 문제 설명
-정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ num_list의 길이 ≤ 1,000
-0 ≤ num_list의 원소 ≤ 1,000
+1 ≤ my_string의 길이 ≤ 1,000
+
 
 
 */
 
-const solution = (num_list) => num_list.reverse();
+const solution = (my_string) => [...my_string].reverse().join("");
