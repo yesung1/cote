608b1c4e72b717ea29607fa39b512b957c6a57de
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-31 14:01:11 +0900

    add:짝수 홀수 개수

--- cote.js
+++ cote.js
@@ -1,26 +1,22 @@
 /*
 
 문제 설명
-"*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다. 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
+정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-1 ≤ n ≤ 10
+1 ≤ num_list의 길이 ≤ 100
+0 ≤ num_list의 원소 ≤ 1,000
 
 */
 
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let input = [];
-
-rl.on("line", function (line) {
-  input = line.split(" ");
-}).on("close", function () {
-  let name = Number(input[0]);
-  for (let x = 1; x <= name; x++) {
-    console.log("*".repeat(x));
-  }
-});
+function solution(num_list) {
+  var answer = [];
+  answer = num_list.reduce(
+    (acc, curr) => {
+      curr % 2 ? (acc[1] += 1) : (acc[0] += 1);
+      return acc;
+    },
+    [0, 0]
+  );
+  return answer;
+}
