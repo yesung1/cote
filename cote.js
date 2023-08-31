aed8be1b0a419323f58f1c65796169cd8a790bbe
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-31 14:02:31 +0900

    add:문자 반복 출력하기

--- cote.js
+++ cote.js
@@ -1,22 +1,14 @@
 /*
 
 문제 설명
-정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-1 ≤ num_list의 길이 ≤ 100
-0 ≤ num_list의 원소 ≤ 1,000
+2 ≤ my_string 길이 ≤ 5
+2 ≤ n ≤ 10
+"my_string"은 영어 대소문자로 이루어져 있습니다.
 
 */
 
-function solution(num_list) {
-  var answer = [];
-  answer = num_list.reduce(
-    (acc, curr) => {
-      curr % 2 ? (acc[1] += 1) : (acc[0] += 1);
-      return acc;
-    },
-    [0, 0]
-  );
-  return answer;
-}
+const solution = (my_string, n) =>
+  [...my_string].map((str) => str.repeat(n)).join("");
