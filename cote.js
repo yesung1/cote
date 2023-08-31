f6e0403a8d082831f3aac8aba28288cb53977e9c
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-08-31 11:27:56 +0900

    add:직각삼각형 출력하기

--- cote.js
+++ cote.js
@@ -1,13 +1,26 @@
 /*
 
 문제 설명
-문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+"*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다. 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 1,000
-
-
+1 ≤ n ≤ 10
 
 */
 
-const solution = (my_string) => [...my_string].reverse().join("");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  let name = Number(input[0]);
+  for (let x = 1; x <= name; x++) {
+    console.log("*".repeat(x));
+  }
+});
