2d859d5a85514e2dcca7e17a02df4617fff0fdd8
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-01 10:10:35 +0900

    add:배열 자르기

--- cote.js
+++ cote.js
@@ -1,14 +1,12 @@
 /*
 
 문제 설명
-정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-0 < n ≤ 1000
+2 ≤ numbers의 길이 ≤ 30
+0 ≤ numbers의 원소 ≤ 1,000
+0 ≤num1 < num2 < numbers의 길이
 
 */
-function solution(n) {
-  var answer = 0;
-  for (let x = 0; x <= n; x += 2) answer += x;
-  return answer;
-}
+const solution = (numbers, num1, num2) => numbers.slice(num1, num2 + 1);
