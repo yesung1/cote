ffcbdc561c4bd647afc38498046b0d3c01338b35
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-01 10:09:22 +0900

    add:각도기

--- cote.js
+++ cote.js
@@ -1,14 +1,16 @@
 /*
 
 문제 설명
-문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다. 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
 
+예각 : 0 < angle < 90
+직각 : angle = 90
+둔각 : 90 < angle < 180
+평각 : angle = 180
 제한사항
-1 ≤ my_string의 길이 ≤ 100
-letter은 길이가 1인 영문자입니다.
-my_string과 letter은 알파벳 대소문자로 이루어져 있습니다.
-대문자와 소문자를 구분합니다.
+0 < angle ≤ 180
+angle은 정수입니다.
 
 */
-const solution = (my_string, letter) =>
-  [...my_string].filter((n) => n !== letter).join("");
+const solution = (angle) =>
+  angle < 90 ? 1 : angle === 90 ? 2 : angle === 180 ? 4 : 3;
