14b3388930c768a9a7baf877a38bceb8f8358962
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-01 10:19:28 +0900

    add:외계행성의 나이

--- cote.js
+++ cote.js
@@ -1,12 +1,12 @@
 /*
 
 문제 설명
-정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+우주여행을 하던 머쓱이는 엔진 고장으로 PROGRAMMERS-962 행성에 불시착하게 됐습니다. 입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다. a는 0, b는 1, c는 2, ..., j는 9입니다. 예를 들어 23살은 cd, 51살은 fb로 표현합니다. 나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-2 ≤ numbers의 길이 ≤ 30
-0 ≤ numbers의 원소 ≤ 1,000
-0 ≤num1 < num2 < numbers의 길이
+age는 자연수입니다.
+age ≤ 1,000
+PROGRAMMERS-962 행성은 알파벳 소문자만 사용합니다.
 
 */
-const solution = (numbers, num1, num2) => numbers.slice(num1, num2 + 1);
+const solution = (age) => [...String(age)].map((n) => "abcdefghij"[n]).join("");
