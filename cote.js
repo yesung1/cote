1973439a1a7114a3d037923b66cc7440b9d46b05
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-04 11:59:57 +0900

    add:배열 회전시키기

--- cote.js
+++ cote.js
@@ -1,16 +1,19 @@
 /*
 
 문제 설명
-머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다. 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다. 친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때, k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
+정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다. 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-2 < numbers의 길이 < 100
-0 < k < 1,000
-numbers의 첫 번째와 마지막 번호는 실제로 바로 옆에 있습니다.
-numbers는 1부터 시작하며 번호는 순서대로 올라갑니다.
+3 ≤ numbers의 길이 ≤ 20
+direction은 "left" 와 "right" 둘 중 하나입니다.
 
 
 
 */
 
-const solution = (numbers, k) => numbers[(2 * (k - 1)) % numbers.length];
+function solution(numbers, direction) {
+  direction === "right"
+    ? numbers.unshift(numbers.pop())
+    : numbers.push(numbers.shift());
+  return numbers;
+}
