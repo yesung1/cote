c24491a1b26eb50dd82d59c0cc0de1485d8a7334
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-04 09:40:34 +0900

    add:구슬을 나누는 경우의 수

--- cote.js
+++ cote.js
@@ -1,14 +1,24 @@
 /*
 
 문제 설명
-가위는 2 바위는 0 보는 5로 표현합니다. 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때, rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다. 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
 
 제한사항
-0 < rsp의 길이 ≤ 100
-rsp와 길이가 같은 문자열을 return 합니다.
-rsp는 숫자 0, 2, 5로 이루어져 있습니다.
+1 ≤ balls ≤ 30
+1 ≤ share ≤ 30
+구슬을 고르는 순서는 고려하지 않습니다.
+share ≤ balls
+
 
 
 */
-const solution = (rsp) =>
-  [...rsp].map((n) => (Number(n) === 2 ? 0 : Number(n) === 0 ? 5 : 2)).join("");
+
+function solution(balls, share) {
+  let answer = 1;
+  while (share) {
+    answer *= balls / share;
+    balls--;
+    share--;
+  }
+  return Math.round(answer);
+}
