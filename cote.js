858dea7e3d9e62c510a32bbb7beb565cb0fd2c0e
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-04 11:40:09 +0900

    add:공 던지기

--- cote.js
+++ cote.js
@@ -1,20 +1,16 @@
 /*
 
 문제 설명
-정수 배열 num_list와 정수 n이 매개변수로 주어집니다. num_list를 다음 설명과 같이 2차원 배열로 바꿔 return하도록 solution 함수를 완성해주세요.
+머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다. 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다. 친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때, k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
 
-num_list가 [1, 2, 3, 4, 5, 6, 7, 8] 로 길이가 8이고 n이 2이므로 num_list를 2 * 4 배열로 다음과 같이 변경합니다. 2차원으로 바꿀 때에는 num_list의 원소들을 앞에서부터 n개씩 나눠 2차원 배열로 변경합니다.
-
-num_list	n	result
-[1, 2, 3, 4, 5, 6, 7, 8]	2	[[1, 2], [3, 4], [5, 6], [7, 8]]
 제한사항
-num_list의 길이는 n의 배 수개입니다.
-0 ≤ num_list의 길이 ≤ 150
-2 ≤ n < num_list의 길이
+2 < numbers의 길이 < 100
+0 < k < 1,000
+numbers의 첫 번째와 마지막 번호는 실제로 바로 옆에 있습니다.
+numbers는 1부터 시작하며 번호는 순서대로 올라갑니다.
 
 
 
 */
 
-const solution = (num_list, n) =>
-  new Array(num_list.length / n).fill([]).map(() => num_list.splice(0, n));
+const solution = (numbers, k) => numbers[(2 * (k - 1)) % numbers.length];
