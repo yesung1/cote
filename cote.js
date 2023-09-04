a818477355b126f9b6c1e5af3ec287b15292b9a6
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-04 15:13:59 +0900

    add:주사위의 개수

--- cote.js
+++ cote.js
@@ -1,19 +1,21 @@
 /*
 
 문제 설명
-정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다. 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
+머쓱이는 직육면체 모양의 상자를 하나 가지고 있는데 이 상자에 정육면체 모양의 주사위를 최대한 많이 채우고 싶습니다. 상자의 가로, 세로, 높이가 저장되어있는 배열 box와 주사위 모서리의 길이 정수 n이 매개변수로 주어졌을 때, 상자에 들어갈 수 있는 주사위의 최대 개수를 return 하도록 solution 함수를 완성해주세요.
 
 제한사항
-3 ≤ numbers의 길이 ≤ 20
-direction은 "left" 와 "right" 둘 중 하나입니다.
+box의 길이는 3입니다.
+box[0] = 상자의 가로 길이
+box[1] = 상자의 세로 길이
+box[2] = 상자의 높이 길이
+1 ≤ box의 원소 ≤ 100
+1 ≤ n ≤ 50
+n ≤ box의 원소
+주사위는 상자와 평행하게 넣습니다.
 
 
 
 */
 
-function solution(numbers, direction) {
-  direction === "right"
-    ? numbers.unshift(numbers.pop())
-    : numbers.push(numbers.shift());
-  return numbers;
-}
+const solution = (box, n) =>
+  box.reduce((acc, curr) => acc * Math.floor(curr / n), 1);
