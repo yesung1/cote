839382b863bdeb0e935da05101c99486b94d3f34
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-04 18:13:18 +0900

    add:팩토리얼

--- cote.js
+++ cote.js
@@ -1,19 +1,24 @@
 /*
 
 문제 설명
-정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다. 예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다. 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
 
+i! ≤ n
 제한사항
-0 ≤ numbers의 원소 ≤ 10,000
-2 ≤ numbers의 길이 ≤ 100
-
-
-
+0 < n ≤ 3,628,800
 
 */
 
-const solution = (numbers) =>
-  numbers
-    .sort((a, b) => a - b)
-    .splice(-2)
-    .reduce((acc, curr) => acc * curr);
+function solution(n) {
+  let answer = 1;
+
+  for (let i = 1; i <= n; i++) {
+    answer *= i;
+    if (answer === n) {
+      return i;
+    }
+    if (answer > n) {
+      return i - 1;
+    }
+  }
+}
