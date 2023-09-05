5bfd8b65c87538cc604351c28f9adbc3fb30510c
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 17:02:50 +0900

    add:369게임

--- cote.js
+++ cote.js
@@ -1,19 +1,13 @@
 /*
 
 문제 설명
-문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+머쓱이는 친구들과 369게임을 하고 있습니다. 369게임은 1부터 숫자를 하나씩 대며 3, 6, 9가 들어가는 숫자는 숫자 대신 3, 6, 9의 개수만큼 박수를 치는 게임입니다. 머쓱이가 말해야하는 숫자 order가 매개변수로 주어질 때, 머쓱이가 쳐야할 박수 횟수를 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-1 < my_string의 길이 < 100
-0 ≤ num1, num2 < my_string의 길이
-my_string은 소문자로 이루어져 있습니다.
-num1 ≠ num2
+1 ≤ order ≤ 1,000,000
 
 
 */
 
-function solution(my_string, num1, num2) {
-  my_string = [...my_string];
-  [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
-  return my_string.join("");
-}
+const solution = (order) =>
+  [...String(order)].filter((o) => ["3", "6", "9"].includes(o)).length;
