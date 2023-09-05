500badd2df52c68cfa021a5a3c58137f2b345e61
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 12:17:37 +0900

    add:숨어있는 숫자의 덧셈 (1)

--- cote.js
+++ cote.js
@@ -1,17 +1,15 @@
 /*
 
 문제 설명
-문자열 my_string이 매개변수로 주어질 때, my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 100
-my_string에는 숫자가 한 개 이상 포함되어 있습니다.
-my_string은 영어 소문자 또는 0부터 9까지의 숫자로 이루어져 있습니다. - - -
+1 ≤ my_string의 길이 ≤ 1,000
+my_string은 소문자, 대문자 그리고 한자리 자연수로만 구성되어있습니다.
 
 */
 
 const solution = (my_string) =>
   [...my_string]
     .filter((str) => !isNaN(str))
-    .map((str) => parseInt(str, 10))
-    .sort((a, b) => a - b);
+    .reduce((acc, curr) => acc + Number(curr), 0);
