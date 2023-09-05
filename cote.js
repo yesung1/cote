f7d1fe7e4635865988532b1b0b1e2ce5cef67ca7
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 12:27:21 +0900

    add:소인수분해

--- cote.js
+++ cote.js
@@ -1,15 +1,20 @@
 /*
 
 문제 설명
-문자열 my_string이 매개변수로 주어집니다. my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다. 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다. 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 1,000
-my_string은 소문자, 대문자 그리고 한자리 자연수로만 구성되어있습니다.
+2 ≤ n ≤ 10,000
 
 */
-
-const solution = (my_string) =>
-  [...my_string]
-    .filter((str) => !isNaN(str))
-    .reduce((acc, curr) => acc + Number(curr), 0);
+function solution(n) {
+  let answer = [];
+  let divisor = 2;
+  while (n >= 2) {
+    if (n % divisor === 0) {
+      answer.push(divisor);
+      n /= divisor;
+    } else divisor++;
+  }
+  return [...new Set(answer)];
+}
