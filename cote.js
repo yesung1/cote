a2ad21bd4fd9d343b774c0568f35331f98e2ef97
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 14:54:58 +0900

    add:배열 원소의 길이

--- cote.js
+++ cote.js
@@ -1,32 +1,14 @@
 /*
 
 문제 설명
-숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 문자열에 있는 숫자를 차례대로 더하려고 합니다. 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다. 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
+문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ s의 길이 ≤ 200
--1,000 < s의 원소 중 숫자 < 1,000
-s는 숫자, "Z", 공백으로 이루어져 있습니다.
-s에 있는 숫자와 "Z"는 서로 공백으로 구분됩니다.
-연속된 공백은 주어지지 않습니다.
-0을 제외하고는 0으로 시작하는 숫자는 없습니다.
-s는 "Z"로 시작하지 않습니다.
-s의 시작과 끝에는 공백이 없습니다.
-"Z"가 연속해서 나오는 경우는 없습니다.
+1 ≤ strlist 원소의 길이 ≤ 100
+strlist는 알파벳 소문자, 대문자, 특수문자로 구성되어 있습니다.
+
 
 
 */
-function solution(s) {
-  var answer = 0;
-  let before = 0;
-  answer = s.split(" ").reduce((acc, curr) => {
-    if (curr === "Z") return acc - before;
-    curr = Number(curr);
-    if (!Number.isNaN(curr)) {
-      before = curr;
-      acc += before;
-    }
-    return acc;
-  }, 0);
-  return answer;
-}
+
+const solution = (strlist) => [...strlist].map((n) => n.length);
