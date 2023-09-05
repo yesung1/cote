7c3c6e53650b434d9d39ec0c0dbf0393f7174241
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 15:12:27 +0900

    add:삼각형의 완성조건 (1)

--- cote.js
+++ cote.js
@@ -1,15 +1,19 @@
 /*
 
 문제 설명
-문자열 my_string이 매개변수로 주어집니다. my_string에서 중복된 문자를 제거하고 하나의 문자만 남긴 문자열을 return하도록 solution 함수를 완성해주세요.
+선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+
+가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ my_string ≤ 110
-my_string은 대문자, 소문자, 공백으로 구성되어 있습니다.
-대문자와 소문자를 구분합니다.
-공백(" ")도 하나의 문자로 구분합니다.
-중복된 문자 중 가장 앞에 있는 문자를 남깁니다.
+sides의 원소는 자연수입니다.
+sides의 길이는 3입니다.
+1 ≤ sides의 원소 ≤ 1,000
 
 */
 
-const solution = (my_string) => [...new Set(my_string)].join("");
+function solution(sides) {
+  sides = sides.sort((a, b) => b - a);
+  return sides[0] >= sides[1] + sides[2] ? 2 : 1;
+}
