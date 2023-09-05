45fe7bf0f85b47e623120b702a70aed1d7a5b248
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 14:54:18 +0900

    add:컨트롤 제트

--- cote.js
+++ cote.js
@@ -1,20 +1,32 @@
 /*
 
 문제 설명
-소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다. 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다. 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 문자열에 있는 숫자를 차례대로 더하려고 합니다. 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다. 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-2 ≤ n ≤ 10,000
+1 ≤ s의 길이 ≤ 200
+-1,000 < s의 원소 중 숫자 < 1,000
+s는 숫자, "Z", 공백으로 이루어져 있습니다.
+s에 있는 숫자와 "Z"는 서로 공백으로 구분됩니다.
+연속된 공백은 주어지지 않습니다.
+0을 제외하고는 0으로 시작하는 숫자는 없습니다.
+s는 "Z"로 시작하지 않습니다.
+s의 시작과 끝에는 공백이 없습니다.
+"Z"가 연속해서 나오는 경우는 없습니다.
+
 
 */
-function solution(n) {
-  let answer = [];
-  let divisor = 2;
-  while (n >= 2) {
-    if (n % divisor === 0) {
-      answer.push(divisor);
-      n /= divisor;
-    } else divisor++;
-  }
-  return [...new Set(answer)];
+function solution(s) {
+  var answer = 0;
+  let before = 0;
+  answer = s.split(" ").reduce((acc, curr) => {
+    if (curr === "Z") return acc - before;
+    curr = Number(curr);
+    if (!Number.isNaN(curr)) {
+      before = curr;
+      acc += before;
+    }
+    return acc;
+  }, 0);
+  return answer;
 }
