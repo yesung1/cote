582f91aac7f2cd3a0decc1440eb1b03eff687738
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 16:49:41 +0900

    add:인덱스 바꾸기

--- cote.js
+++ cote.js
@@ -1,19 +1,19 @@
 /*
 
 문제 설명
-선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
-
-가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
-삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
+문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-sides의 원소는 자연수입니다.
-sides의 길이는 3입니다.
-1 ≤ sides의 원소 ≤ 1,000
+1 < my_string의 길이 < 100
+0 ≤ num1, num2 < my_string의 길이
+my_string은 소문자로 이루어져 있습니다.
+num1 ≠ num2
+
 
 */
 
-function solution(sides) {
-  sides = sides.sort((a, b) => b - a);
-  return sides[0] >= sides[1] + sides[2] ? 2 : 1;
+function solution(my_string, num1, num2) {
+  my_string = [...my_string];
+  [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+  return my_string.join("");
 }
