a09c8f28b50411815de25246c179240eeb65e7a6
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-05 11:47:15 +0900

    add:문자열 정렬하기 (1)

--- cote.js
+++ cote.js
@@ -1,13 +1,17 @@
 /*
 
 문제 설명
-영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다. 문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+문자열 my_string이 매개변수로 주어질 때, my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
 
 제한사항
-my_string은 소문자와 공백으로 이루어져 있습니다.
-1 ≤ my_string의 길이 ≤ 1,000
+1 ≤ my_string의 길이 ≤ 100
+my_string에는 숫자가 한 개 이상 포함되어 있습니다.
+my_string은 영어 소문자 또는 0부터 9까지의 숫자로 이루어져 있습니다. - - -
 
 */
 
 const solution = (my_string) =>
-  [...my_string].filter((n) => !["a", "e", "i", "o", "u"].includes(n)).join("");
+  [...my_string]
+    .filter((str) => !isNaN(str))
+    .map((str) => parseInt(str, 10))
+    .sort((a, b) => a - b);
