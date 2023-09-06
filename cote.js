cbfd65c4b3203c5293f59745473fac744a59c58a
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-06 11:00:29 +0900

    add:영어가 싫어요

--- cote.js
+++ cote.js
@@ -1,20 +1,32 @@
 /*
 
+
 문제 설명
-문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다. 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 1,000
-my_string은 영어 대문자와 소문자로만 구성되어 있습니다.
+numbers는 소문자로만 구성되어 있습니다.
+numbers는 "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" 들이 공백 없이 조합되어 있습니다.
+1 ≤ numbers의 길이 ≤ 50
+"zero"는 numbers의 맨 앞에 올 수 없습니다.
 
 */
 
-const solution = (my_string) =>
-  [...my_string]
-    .map((str) => {
-      str === str.toUpperCase()
-        ? (str = str.toLowerCase())
-        : (str = str.toUpperCase());
-      return str;
-    })
-    .join("");
+function solution(numbers) {
+  let strArr = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  strArr.forEach((str, idx) => {
+    numbers = numbers.replaceAll(str, idx);
+  });
+  return Number(numbers);
+}
