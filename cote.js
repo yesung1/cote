805a95c7f5e55f1ec27f2205f6a25b989463e7c0
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-06 10:46:37 +0900

    add:대문자와 소문자

--- cote.js
+++ cote.js
@@ -1,20 +1,20 @@
 /*
 
 문제 설명
-군 전략가 머쓱이는 전쟁 중 적군이 다음과 같은 암호 체계를 사용한다는 것을 알아냈습니다.
-
-암호화된 문자열 cipher를 주고받습니다.
-그 문자열에서 code의 배수 번째 글자만 진짜 암호입니다.
-문자열 cipher와 정수 code가 매개변수로 주어질 때 해독된 암호 문자열을 return하도록 solution 함수를 완성해주세요.
+문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ cipher의 길이 ≤ 1,000
-1 ≤ code ≤ cipher의 길이
-cipher는 소문자와 공백으로만 구성되어 있습니다.
-공백도 하나의 문자로 취급합니다.
-
+1 ≤ my_string의 길이 ≤ 1,000
+my_string은 영어 대문자와 소문자로만 구성되어 있습니다.
 
 */
 
-const solution = (cipher, code) =>
-  [...cipher].filter((_, i) => (i + 1) % code === 0).join("");
+const solution = (my_string) =>
+  [...my_string]
+    .map((str) => {
+      str === str.toUpperCase()
+        ? (str = str.toLowerCase())
+        : (str = str.toUpperCase());
+      return str;
+    })
+    .join("");
