84c2a0db54de8eae88e86ae8268ae58db7ea2f24
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-07 11:03:35 +0900

    add:문자열 계산하기

--- cote.js
+++ cote.js
@@ -2,16 +2,22 @@
 
 
 문제 설명
-정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+my_string은 "3 + 5"처럼 문자열로 된 수식입니다. 문자열 my_string이 매개변수로 주어질 때, 수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ array의 길이 ≤ 100
-0 ≤ array 원소 ≤ 1,000
-array에 중복된 숫자는 없습니다.
+연산자는 +, -만 존재합니다.
+문자열의 시작과 끝에는 공백이 없습니다.
+0으로 시작하는 숫자는 주어지지 않습니다.
+잘못된 수식은 주어지지 않습니다.
+5 ≤ my_string의 길이 ≤ 100
+my_string을 계산한 결과값은 1 이상 100,000 이하입니다.
+my_string의 중간 계산 값은 -100,000 이상 100,000 이하입니다.
+계산에 사용하는 숫자는 1 이상 20,000 이하인 자연수입니다.
+my_string에는 연산자가 적어도 하나 포함되어 있습니다.
+return type 은 정수형입니다.
+my_string의 숫자와 연산자는 공백 하나로 구분되어 있습니다.
 
 
 */
 
-function solution(array) {
-  return [Math.max(...array), array.indexOf(Math.max(...array))];
-}
+const solution = (my_string) => new Function("return " + my_string)();
