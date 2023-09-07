53d218f56e1ee36b3773624ed58811408090d682
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-07 11:02:13 +0900

    add:가장 큰 수 찾기

--- cote.js
+++ cote.js
@@ -2,15 +2,16 @@
 
 
 문제 설명
-머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 적으려고 하며, 편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로길이를 return 하도록 solution 함수를 완성해주세요.
+정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-공백도 하나의 문자로 취급합니다.
-1 ≤ message의 길이 ≤ 50
-편지지의 여백은 생각하지 않습니다.
-message는 영문 알파벳 대소문자, ‘!’, ‘~’ 또는 공백으로만 이루어져 있습니다.
+1 ≤ array의 길이 ≤ 100
+0 ≤ array 원소 ≤ 1,000
+array에 중복된 숫자는 없습니다.
 
 
 */
 
-const solution = (message) => message.length * 2;
+function solution(array) {
+  return [Math.max(...array), array.indexOf(Math.max(...array))];
+}
