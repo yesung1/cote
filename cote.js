ab619c70f514072fb7ce3cba41df1997d6b39b21
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-07 11:05:34 +0900

    add:n의 배수 고르기

--- cote.js
+++ cote.js
@@ -2,17 +2,13 @@
 
 
 문제 설명
-정수 num과 k가 매개변수로 주어질 때, num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return하고 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+정수 n과 정수 배열 numlist가 매개변수로 주어질 때, numlist에서 n의 배수가 아닌 수들을 제거한 배열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 < num < 1,000,000
-0 ≤ k < 10
-num에 k가 여러 개 있으면 가장 처음 나타나는 자리를 return 합니다.
+1 ≤ n ≤ 10,000
+1 ≤ numlist의 크기 ≤ 100
+1 ≤ numlist의 원소 ≤ 100,000
 
 */
 
-function solution(num, k) {
-  var answer = 0;
-  answer = String(num).indexOf(k);
-  return answer >= 0 ? answer + 1 : -1;
-}
+const solution = (n, numlist) => numlist.filter((num) => num % n === 0);
