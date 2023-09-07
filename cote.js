6ee6d31ef840baeb2d898152e7ffba1b69a922d0
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-07 11:01:14 +0900

    add:편지

--- cote.js
+++ cote.js
@@ -2,18 +2,15 @@
 
 
 문제 설명
-정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 적으려고 하며, 편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로길이를 return 하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ n ≤ 10,000
+공백도 하나의 문자로 취급합니다.
+1 ≤ message의 길이 ≤ 50
+편지지의 여백은 생각하지 않습니다.
+message는 영문 알파벳 대소문자, ‘!’, ‘~’ 또는 공백으로만 이루어져 있습니다.
 
 
 */
 
-function solution(n) {
-  let answer = [];
-  for (let x = 0; x <= n; x++) {
-    if (n % x === 0) answer.push(x);
-  }
-  return answer;
-}
+const solution = (message) => message.length * 2;
