ecf60330e275806789081d5176a5d1c61126e0ca
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-08 18:00:36 +0900

    add:잘라서 배열로 저장하기

--- cote.js
+++ cote.js
@@ -3,14 +3,21 @@
 
 
 문제 설명
-머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ array의 길이 ≤ 100
-0 ≤ array의 원소 ≤ 100,000
+1 ≤ my_str의 길이 ≤ 100
+1 ≤ n ≤ my_str의 길이
+my_str은 알파벳 소문자, 대문자, 숫자로 이루어져 있습니다.
 
 
 
 */
 
-const solution = (array) => array.join("").split(7).length - 1;
+function solution(my_str, n) {
+  var answer = [];
+  for (let x = 0; x < my_str.length; x += n) {
+    answer.push(my_str.slice(x, x + n));
+  }
+  return answer;
+}
