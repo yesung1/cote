e2c917bb9742959a51c6498dbc65ebc39052be94
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-08 16:50:17 +0900

    add:문자열 정렬하기 (2)

--- cote.js
+++ cote.js
@@ -3,13 +3,13 @@
 
 
 문제 설명
-어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때, my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-1 ≤ n ≤ 10
-1 ≤ t ≤ 15
+0 < my_string 길이 < 100
 
 
 */
 
-const solution = (n, t) => Math.pow(2, t) * n;
+const solution = (my_string) =>
+  my_string.toLowerCase().split("").sort().join("");
