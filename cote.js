59e2a3a423153328a98e50b55b6875509e4086e9
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-08 18:01:40 +0900

    add:머쓱이보다 키 큰 사람

--- cote.js
+++ cote.js
@@ -3,15 +3,15 @@
 
 
 문제 설명
-정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+머쓱이는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
 1 ≤ array의 길이 ≤ 100
-0 ≤ array의 원소 ≤ 1,000
-0 ≤ n ≤ 1,000
+1 ≤ height ≤ 200
+1 ≤ array의 원소 ≤ 200
 
 
 
 */
 
-const solution = (array, n) => array.filter((arr) => arr === n).length;
+const solution = (array, height) => array.filter((n) => n > height).length;
