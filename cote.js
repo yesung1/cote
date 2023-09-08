ce531088637b39b3e3114a378cec31102e9cacea
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-08 18:00:12 +0900

    add:7의 개수

--- cote.js
+++ cote.js
@@ -3,13 +3,14 @@
 
 
 문제 설명
-영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때, my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-0 < my_string 길이 < 100
+1 ≤ array의 길이 ≤ 100
+0 ≤ array의 원소 ≤ 100,000
+
 
 
 */
 
-const solution = (my_string) =>
-  my_string.toLowerCase().split("").sort().join("");
+const solution = (array) => array.join("").split(7).length - 1;
