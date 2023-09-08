9be57707eb49c9193a30a413b920023a11bbaaef
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-08 16:23:57 +0900

    add:문자열안에 문자열

--- cote.js
+++ cote.js
@@ -3,27 +3,16 @@
 
 
 문제 설명
-덧셈, 뺄셈 수식들이 'X [연산자] Y = Z' 형태로 들어있는 문자열 배열 quiz가 매개변수로 주어집니다. 수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+문자열 str1, str2가 매개변수로 주어집니다. str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-연산 기호와 숫자 사이는 항상 하나의 공백이 존재합니다. 단 음수를 표시하는 마이너스 기호와 숫자 사이에는 공백이 존재하지 않습니다.
-1 ≤ quiz의 길이 ≤ 10
-X, Y, Z는 각각 0부터 9까지 숫자로 이루어진 정수를 의미하며, 각 숫자의 맨 앞에 마이너스 기호가 하나 있을 수 있고 이는 음수를 의미합니다.
-X, Y, Z는 0을 제외하고는 0으로 시작하지 않습니다.
--10,000 ≤ X, Y ≤ 10,000
--20,000 ≤ Z ≤ 20,000
-[연산자]는 + 와 - 중 하나입니다.
+1 ≤ str1의 길이 ≤ 100
+1 ≤ str2의 길이 ≤ 100
+문자열은 알파벳 대문자, 소문자, 숫자로 구성되어 있습니다.
+
 
 
 
 */
 
-function solution(quiz) {
-  var answer = [];
-  return quiz.map((t) => {
-    const [buho, result] = t.split(" = ");
-    const sign = buho.includes("+") ? 1 : -1;
-    const [a, b] = buho.split(sign === 1 ? " + " : " - ");
-    return +a + +b * sign === +result ? "O" : "X";
-  });
-}
+const solution = (str1, str2) => (str1.includes(str2) ? 1 : 2);
