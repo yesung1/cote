919407d1a529b468056d1249c7cae432fb1c3a39
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-08 16:49:30 +0900

    add:제곱수 판별하기

--- cote.js
+++ cote.js
@@ -3,16 +3,13 @@
 
 
 문제 설명
-문자열 str1, str2가 매개변수로 주어집니다. str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ str1의 길이 ≤ 100
-1 ≤ str2의 길이 ≤ 100
-문자열은 알파벳 대문자, 소문자, 숫자로 구성되어 있습니다.
-
+1 ≤ n ≤ 1,000,000
 
 
 
 */
 
-const solution = (str1, str2) => (str1.includes(str2) ? 1 : 2);
+const solution = (n) => (Number.isInteger(Math.pow(n, 0.5)) ? 1 : 2);
