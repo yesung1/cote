d7f3ebf35fa211387c87b09099d8647f94ad2b02
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-13 15:26:52 +0900

    add:직사각형 넓이 구하기

--- cote.js
+++ cote.js
@@ -3,15 +3,25 @@
 
 
 문제 설명
-머쓱이는 학교에서 키 순으로 줄을 설 때 몇 번째로 서야 하는지 궁금해졌습니다. 머쓱이네 반 친구들의 키가 담긴 정수 배열 array와 머쓱이의 키 height가 매개변수로 주어질 때, 머쓱이보다 키 큰 사람 수를 return 하도록 solution 함수를 완성해보세요.
+2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다. 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-1 ≤ array의 길이 ≤ 100
-1 ≤ height ≤ 200
-1 ≤ array의 원소 ≤ 200
+dots의 길이 = 4
+dots의 원소의 길이 = 2
+-256 < dots[i]의 원소 < 256
+잘못된 입력은 주어지지 않습니다.
 
 
 
 */
 
-const solution = (array, height) => array.filter((n) => n > height).length;
+function solution(dots) {
+  var answer = 0;
+  let w = [];
+  let h = [];
+  dots.map(([a, b]) => {
+    w.push(a);
+    h.push(b);
+  });
+  return (Math.max(...w) - Math.min(...w)) * (Math.max(...h) - Math.min(...h));
+}
