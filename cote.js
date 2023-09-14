d82794dd7c77d60cf33b2beaba0009cdee3ada5d
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-14 11:16:27 +0900

    add: 캐릭터의 좌표

--- cote.js
+++ cote.js
@@ -3,25 +3,41 @@
 
 
 문제 설명
-2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다. 직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+머쓱이는 RPG게임을 하고 있습니다. 게임에는 up, down, left, right 방향키가 있으며 각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다. 예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1], down을 누른다면 [0, -1], left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다. 머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다. 캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
 
+[0, 0]은 board의 정 중앙에 위치합니다. 예를 들어 board의 가로 크기가 9라면 캐릭터는 왼쪽으로 최대 [-4, 0]까지 오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
 제한사항
-dots의 길이 = 4
-dots의 원소의 길이 = 2
--256 < dots[i]의 원소 < 256
-잘못된 입력은 주어지지 않습니다.
+board은 [가로 크기, 세로 크기] 형태로 주어집니다.
+board의 가로 크기와 세로 크기는 홀수입니다.
+board의 크기를 벗어난 방향키 입력은 무시합니다.
+0 ≤ keyinput의 길이 ≤ 50
+1 ≤ board[0] ≤ 99
+1 ≤ board[1] ≤ 99
+keyinput은 항상 up, down, left, right만 주어집니다.
 
 
 
 */
 
-function solution(dots) {
-  var answer = 0;
-  let w = [];
-  let h = [];
-  dots.map(([a, b]) => {
-    w.push(a);
-    h.push(b);
+function solution(keyinput, board) {
+  var answer = [0, 0];
+  board[0] = (board[0] - 1) / 2 - 1;
+  board[1] = (board[1] - 1) / 2 - 1;
+  keyinput.map((key) => {
+    switch (key) {
+      case "left":
+        if (board[0] >= -answer[0]) answer[0]--;
+        break;
+      case "right":
+        if (board[0] >= answer[0]) answer[0]++;
+        break;
+      case "up":
+        if (board[1] >= answer[1]) answer[1]++;
+        break;
+      case "down":
+        if (board[1] >= -answer[1]) answer[1]--;
+        break;
+    }
   });
-  return (Math.max(...w) - Math.min(...w)) * (Math.max(...h) - Math.min(...h));
+  return answer;
 }
