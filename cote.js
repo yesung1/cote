f9eddfc6143e6dfd20b257226a3e16cbacf3387f
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-15 10:47:28 +0900

    add:다항식 더하기

--- cote.js
+++ cote.js
@@ -1,24 +1,42 @@
 /*
 
 
-
 문제 설명
-정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다. 다항식을 계산할 때는 동류항끼리 계산해 정리합니다. 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때, 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요. 같은 식이라면 가장 짧은 수식을 return 합니다.
 
 제한사항
--10,000 ≤ numbers의 원소 ≤ 10,000
-2 ≤ numbers 의 길이 ≤ 100
-
+0 < polynomial에 있는 수 < 100
+polynomial에 변수는 'x'만 존재합니다.
+polynomial은 양의 정수, 공백, ‘x’, ‘+'로 이루어져 있습니다.
+항과 연산기호 사이에는 항상 공백이 존재합니다.
+공백은 연속되지 않으며 시작이나 끝에는 공백이 없습니다.
+하나의 항에서 변수가 숫자 앞에 오는 경우는 없습니다.
+" + 3xx + + x7 + "와 같은 잘못된 입력은 주어지지 않습니다.
+0으로 시작하는 수는 없습니다.
+문자와 숫자 사이의 곱하기는 생략합니다.
+polynomial에는 일차 항과 상수항만 존재합니다.
+계수 1은 생략합니다.
+결괏값에 상수항은 마지막에 둡니다.
+0 < polynomial의 길이 < 50
 
 
 */
 
-function solution(numbers) {
-  var answer = [];
-  for (let i = 0; i < numbers.length - 1; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      answer.push(numbers[i] * numbers[j]);
+function solution(polynomial) {
+  const arr = polynomial.split(" + ");
+  let answer = [];
+  let xNum = 0;
+  let num = 0;
+
+  arr.forEach((n) => {
+    if (n.includes("x")) {
+      const strVal = n.replace("x", "") || "1";
+      xNum += parseInt(strVal, 10);
+    } else {
+      num += parseInt(n);
     }
-  }
-  return Math.max(...answer);
+  });
+  if (xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
+  if (num) answer.push(num);
+  return answer.join(" + ");
 }
