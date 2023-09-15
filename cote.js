bdeb1200122edd1d03216af031ceb41c4cd303b1
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-15 18:18:39 +0900

    add:안전지대

--- cote.js
+++ cote.js
@@ -2,17 +2,35 @@
 
 
 문제 설명
-문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+다음 그림과 같이 지뢰가 있는 지역과 지뢰에 인접한 위, 아래, 좌, 우 대각선 칸을 모두 위험지역으로 분류합니다.
+image.png
+지뢰는 2차원 배열 board에 1로 표시되어 있고 board에는 지뢰가 매설 된 지역 1과, 지뢰가 없는 지역 0만 존재합니다.
+지뢰가 매설된 지역의 지도 board가 매개변수로 주어질 때, 안전한 지역의 칸 수를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-1 ≤ my_string의 길이 ≤ 1,000
-1 ≤ my_string 안의 자연수 ≤ 1000
-연속된 수는 하나의 숫자로 간주합니다.
-000123과 같이 0이 선행하는 경우는 없습니다.
-문자열에 자연수가 없는 경우 0을 return 해주세요.
+board는 n * n 배열입니다.
+1 ≤ n ≤ 100
+지뢰는 1로 표시되어 있습니다.
+board에는 지뢰가 있는 지역 1과 지뢰가 없는 지역 0만 존재합니다.
 
 
 */
 
-const solution = (my_string) =>
-  my_string.split(/[a-zA-Z]/g).reduce((a, c) => a + Number(c), 0);
+function solution(board) {
+  var answer = 0;
+  let board2 = JSON.parse(JSON.stringify(board));
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[i][j] === 1) {
+        board2[i - 1 >= 0 ? i - 1 : i].fill(1, j - 1 >= 0 ? j - 1 : j, j + 2);
+        board2[i].fill(1, j - 1 >= 0 ? j - 1 : j, j + 2);
+        board2[i + 1 >= board.length ? i : i + 1].fill(
+          1,
+          j - 1 >= 0 ? j - 1 : j,
+          j + 2
+        );
+      }
+    }
+  }
+  return (answer = board2.flat().filter((n) => !n).length);
+}
