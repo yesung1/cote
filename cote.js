c83cd05ef80b3842621f3aed33f9f38dbf67dc67
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-15 10:53:41 +0900

    add:숨어있는 숫자의 덧셈 (2)

--- cote.js
+++ cote.js
@@ -2,41 +2,17 @@
 
 
 문제 설명
-한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다. 다항식을 계산할 때는 동류항끼리 계산해 정리합니다. 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때, 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요. 같은 식이라면 가장 짧은 수식을 return 합니다.
+문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 < polynomial에 있는 수 < 100
-polynomial에 변수는 'x'만 존재합니다.
-polynomial은 양의 정수, 공백, ‘x’, ‘+'로 이루어져 있습니다.
-항과 연산기호 사이에는 항상 공백이 존재합니다.
-공백은 연속되지 않으며 시작이나 끝에는 공백이 없습니다.
-하나의 항에서 변수가 숫자 앞에 오는 경우는 없습니다.
-" + 3xx + + x7 + "와 같은 잘못된 입력은 주어지지 않습니다.
-0으로 시작하는 수는 없습니다.
-문자와 숫자 사이의 곱하기는 생략합니다.
-polynomial에는 일차 항과 상수항만 존재합니다.
-계수 1은 생략합니다.
-결괏값에 상수항은 마지막에 둡니다.
-0 < polynomial의 길이 < 50
+1 ≤ my_string의 길이 ≤ 1,000
+1 ≤ my_string 안의 자연수 ≤ 1000
+연속된 수는 하나의 숫자로 간주합니다.
+000123과 같이 0이 선행하는 경우는 없습니다.
+문자열에 자연수가 없는 경우 0을 return 해주세요.
 
 
 */
 
-function solution(polynomial) {
-  const arr = polynomial.split(" + ");
-  let answer = [];
-  let xNum = 0;
-  let num = 0;
-
-  arr.forEach((n) => {
-    if (n.includes("x")) {
-      const strVal = n.replace("x", "") || "1";
-      xNum += parseInt(strVal, 10);
-    } else {
-      num += parseInt(n);
-    }
-  });
-  if (xNum) answer.push(`${xNum === 1 ? "" : xNum}x`);
-  if (num) answer.push(num);
-  return answer.join(" + ");
-}
+const solution = (my_string) =>
+  my_string.split(/[a-zA-Z]/g).reduce((a, c) => a + Number(c), 0);
