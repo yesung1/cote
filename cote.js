128807ee215d1b499d4c7ae3d32ec7281d5927c2
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-18 12:28:02 +0900

    add:외계어 사전

--- cote.js
+++ cote.js
@@ -2,28 +2,21 @@
 
 
 문제 설명
-선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
-
-가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
-삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
+PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-sides의 원소는 자연수입니다.
-sides의 길이는 2입니다.
-1 ≤ sides의 원소 ≤ 1,000
+spell과 dic의 원소는 알파벳 소문자로만 이루어져있습니다.
+2 ≤ spell의 크기 ≤ 10
+spell의 원소의 길이는 1입니다.
+1 ≤ dic의 크기 ≤ 10
+1 ≤ dic의 원소의 길이 ≤ 10
+spell의 원소를 모두 사용해 단어를 만들어야 합니다.
+spell의 원소를 모두 사용해 만들 수 있는 단어는 dic에 두 개 이상 존재하지 않습니다.
+dic과 spell 모두 중복된 원소를 갖지 않습니다.
+
 
 
 */
 
-function solution(sides) {
-  let count = 0;
-  const max = Math.max(...sides);
-  const min = Math.min(...sides);
-  for (let i = max - min + 1; i <= max; i++) {
-    count++;
-  }
-  for (let i = max + 1; i < max + min; i++) {
-    count++;
-  }
-  return count;
-}
+const solution = (spell, dic) =>
+  dic.filter((v) => spell.every((s) => v.includes(s))).length ? 1 : 2;
