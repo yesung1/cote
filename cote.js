4b1d7611d8661a171d52dc0758869b9e7c20b148
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-18 16:17:19 +0900

    add:저주의 숫자 3

--- cote.js
+++ cote.js
@@ -2,21 +2,27 @@
 
 
 문제 설명
-PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+3x 마을 사람들은 3을 저주의 숫자라고 생각하기 때문에 3의 배수와 숫자 3을 사용하지 않습니다. 3x 마을 사람들의 숫자는 다음과 같습니다.
+
+10진법	3x 마을에서 쓰는 숫자	10진법	3x 마을에서 쓰는 숫자
+1	1	6	8
+2	2	7	10
+3	4	8	11
+4	5	9	14
+5	7	10	16
+정수 n이 매개변수로 주어질 때, n을 3x 마을에서 사용하는 숫자로 바꿔 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-spell과 dic의 원소는 알파벳 소문자로만 이루어져있습니다.
-2 ≤ spell의 크기 ≤ 10
-spell의 원소의 길이는 1입니다.
-1 ≤ dic의 크기 ≤ 10
-1 ≤ dic의 원소의 길이 ≤ 10
-spell의 원소를 모두 사용해 단어를 만들어야 합니다.
-spell의 원소를 모두 사용해 만들 수 있는 단어는 dic에 두 개 이상 존재하지 않습니다.
-dic과 spell 모두 중복된 원소를 갖지 않습니다.
-
+1 ≤ n ≤ 100
 
 
 */
 
-const solution = (spell, dic) =>
-  dic.filter((v) => spell.every((s) => v.includes(s))).length ? 1 : 2;
+function solution(n) {
+  var answer = 0;
+  for (let i = 0; i < n; i++) {
+    answer++;
+    while (answer % 3 === 0 || answer.toString().includes("3")) answer++;
+  }
+  return answer;
+}
