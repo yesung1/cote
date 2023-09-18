2140f00656d1ce2e20b87f8cca0501afa06ef7c9
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-18 11:51:42 +0900

    add:삼각형의 완성조건 (2)

--- cote.js
+++ cote.js
@@ -2,35 +2,29 @@
 
 
 문제 설명
-다음 그림과 같이 지뢰가 있는 지역과 지뢰에 인접한 위, 아래, 좌, 우 대각선 칸을 모두 위험지역으로 분류합니다.
-image.png
-지뢰는 2차원 배열 board에 1로 표시되어 있고 board에는 지뢰가 매설 된 지역 1과, 지뢰가 없는 지역 0만 존재합니다.
-지뢰가 매설된 지역의 지도 board가 매개변수로 주어질 때, 안전한 지역의 칸 수를 return하도록 solution 함수를 완성해주세요.
+선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+
+가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-board는 n * n 배열입니다.
-1 ≤ n ≤ 100
-지뢰는 1로 표시되어 있습니다.
-board에는 지뢰가 있는 지역 1과 지뢰가 없는 지역 0만 존재합니다.
+sides의 원소는 자연수입니다.
+sides의 길이는 2입니다.
+1 ≤ sides의 원소 ≤ 1,000
+
 
 
 */
 
-function solution(board) {
-  var answer = 0;
-  let board2 = JSON.parse(JSON.stringify(board));
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      if (board[i][j] === 1) {
-        board2[i - 1 >= 0 ? i - 1 : i].fill(1, j - 1 >= 0 ? j - 1 : j, j + 2);
-        board2[i].fill(1, j - 1 >= 0 ? j - 1 : j, j + 2);
-        board2[i + 1 >= board.length ? i : i + 1].fill(
-          1,
-          j - 1 >= 0 ? j - 1 : j,
-          j + 2
-        );
-      }
-    }
+function solution(sides) {
+  let count = 0;
+  const max = Math.max(...sides);
+  const min = Math.min(...sides);
+  for (let i = max - min + 1; i <= max; i++) {
+    count++;
   }
-  return (answer = board2.flat().filter((n) => !n).length);
+  for (let i = max + 1; i < max + min; i++) {
+    count++;
+  }
+  return count;
 }
