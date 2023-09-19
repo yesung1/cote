f069b16871c1cc02bb413d7001bfeddba07d4e07
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-19 11:41:42 +0900

    add:겹치는 선분의 길이

--- cote.js
+++ cote.js
@@ -2,27 +2,30 @@
 
 
 문제 설명
-3x 마을 사람들은 3을 저주의 숫자라고 생각하기 때문에 3의 배수와 숫자 3을 사용하지 않습니다. 3x 마을 사람들의 숫자는 다음과 같습니다.
+선분 3개가 평행하게 놓여 있습니다. 세 선분의 시작과 끝 좌표가 [[start, end], [start, end], [start, end]] 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때, 두 개 이상의 선분이 겹치는 부분의 길이를 return 하도록 solution 함수를 완성해보세요.
 
-10진법	3x 마을에서 쓰는 숫자	10진법	3x 마을에서 쓰는 숫자
-1	1	6	8
-2	2	7	10
-3	4	8	11
-4	5	9	14
-5	7	10	16
-정수 n이 매개변수로 주어질 때, n을 3x 마을에서 사용하는 숫자로 바꿔 return하도록 solution 함수를 완성해주세요.
+lines가 [[0, 2], [-3, -1], [-2, 1]]일 때 그림으로 나타내면 다음과 같습니다.
+
+line_2.png
+
+선분이 두 개 이상 겹친 곳은 [-2, -1], [0, 1]로 길이 2만큼 겹쳐있습니다.
 
 제한사항
-1 ≤ n ≤ 100
+lines의 길이 = 3
+lines의 원소의 길이 = 2
+모든 선분은 길이가 1 이상입니다.
+lines의 원소는 [a, b] 형태이며, a, b는 각각 선분의 양 끝점 입니다.
+-100 ≤ a < b ≤ 100
 
 
 */
 
-function solution(n) {
-  var answer = 0;
-  for (let i = 0; i < n; i++) {
-    answer++;
-    while (answer % 3 === 0 || answer.toString().includes("3")) answer++;
-  }
-  return answer;
+function solution(lines) {
+  let line = new Array(200).fill(0);
+
+  lines.forEach(([a, b]) => {
+    for (; a < b; a++) line[a + 100]++;
+  });
+
+  return line.reduce((acc, curr) => (curr > 1 ? acc + 1 : acc), 0);
 }
