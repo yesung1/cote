a84e323db55ef55ae67659554dcbb9e9a1ba3485
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-09-25 12:27:53 +0900

    add:등수 매기기

--- cote.js
+++ cote.js
@@ -1,6 +1,13 @@
 /*
 
 문제 설명
+영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+0 ≤ score[0], score[1] ≤ 100
+1 ≤ score의 길이 ≤ 10
+score의 원소 길이는 2입니다.
+score는 중복된 원소를 갖지 않습니다.
 정수 n을 기준으로 n과 가까운 수부터 정렬하려고 합니다. 이때 n으로부터의 거리가 같다면 더 큰 수를 앞에 오도록 배치합니다. 정수가 담긴 배열 numlist와 정수 n이 주어질 때 numlist의 원소를 n으로부터 가까운 순서대로 정렬한 배열을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
@@ -12,5 +19,8 @@ numlist는 중복된 원소를 갖지 않습니다.
 
 */
 
-const solution = (numlist, n) =>
-  numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
+function solution(score) {
+  let sum = score.map((v) => v[0] + v[1]);
+  let sumArr = sum.slice().sort((a, b) => b - a);
+  return sum.map((v) => sumArr.indexOf(v) + 1);
+}
