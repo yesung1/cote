532c8170be9551d3faec4be8a3512349b4fc9e14
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-06 11:53:34 +0900

    add:이진수 더하기

--- cote.js
+++ cote.js
@@ -1,20 +1,17 @@
 /*
 
 문제 설명
-프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
 
 제한사항
-chicken은 정수입니다.
-0 ≤ chicken ≤ 1,000,000
+return 값은 이진수를 의미하는 문자열입니다.
+1 ≤ bin1, bin2의 길이 ≤ 10
+bin1과 bin2는 0과 1로만 이루어져 있습니다.
+bin1과 bin2는 "0"을 제외하고 0으로 시작하지 않습니다.
+
 
 
 */
 
-function solution(chicken) {
-  let answer = 0;
-  while (chicken >= 10) {
-    answer += Math.floor(chicken / 10);
-    chicken = (chicken % 10) + Math.floor(chicken / 10);
-  }
-  return answer;
-}
+const solution = (bin1, bin2) =>
+  (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
