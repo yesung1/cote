c21f47d77720a70a9c1ba24aac5ce6b3fa782083
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-06 11:59:59 +0900

    add:k의 개수

--- cote.js
+++ cote.js
@@ -1,18 +1,21 @@
 /*
 
 문제 설명
-문자열 before와 after가 매개변수로 주어질 때, before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.
+1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장합니다. 정수 i, j, k가 매개변수로 주어질 때, i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
 
 제한사항
-0 < before의 길이 == after의 길이 < 1,000
-before와 after는 모두 소문자로 이루어져 있습니다.
+1 ≤ i < j ≤ 100,000
+0 ≤ k ≤ 9
+
 
 
 
 */
 
-function solution(before, after) {
-  return before.split("").sort().join("") === after.split("").sort().join("")
-    ? 1
-    : 0;
+function solution(i, j, k) {
+  var answer = 0;
+  for (i; i <= j; i++) {
+    answer += [...String(i)].filter((n) => Number(n) === k).length;
+  }
+  return answer;
 }
