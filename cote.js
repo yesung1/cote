f535a7f58e5f5c0201140cebf2961e026aeae749
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-06 16:51:06 +0900

    add:문자열 밀기

--- cote.js
+++ cote.js
@@ -1,21 +1,15 @@
 /*
 
 문제 설명
-1부터 13까지의 수에서, 1은 1, 10, 11, 12, 13 이렇게 총 6번 등장합니다. 정수 i, j, k가 매개변수로 주어질 때, i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
+문자열 "hello"에서 각 문자를 오른쪽으로 한 칸씩 밀고 마지막 문자는 맨 앞으로 이동시키면 "ohell"이 됩니다. 이것을 문자열을 민다고 정의한다면 문자열 A와 B가 매개변수로 주어질 때, A를 밀어서 B가 될 수 있다면 밀어야 하는 최소 횟수를 return하고 밀어서 B가 될 수 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-1 ≤ i < j ≤ 100,000
-0 ≤ k ≤ 9
+0 < A의 길이 = B의 길이 < 100
+A, B는 알파벳 소문자로 이루어져 있습니다.
 
 
 
 
 */
 
-function solution(i, j, k) {
-  var answer = 0;
-  for (i; i <= j; i++) {
-    answer += [...String(i)].filter((n) => Number(n) === k).length;
-  }
-  return answer;
-}
+const solution = (A, B) => (B + B).indexOf(A);
