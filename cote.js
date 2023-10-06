a5e27c16dde441dd8776218af956e3c401f681ef
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-06 17:33:49 +0900

    add:짝수와 홀수

--- cote.js
+++ cote.js
@@ -1,23 +1,14 @@
 /*
 
 문제 설명
-등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
 
-제한사항
-2 < common의 길이 < 1,000
--1,000 < common의 원소 < 2,000
-common의 원소는 모두 정수입니다.
-등차수열 혹은 등비수열이 아닌 경우는 없습니다.
-등비수열인 경우 공비는 0이 아닌 정수입니다.
+제한 조건
+num은 int 범위의 정수입니다.
+0은 짝수입니다.
+
 
 
 */
 
-function solution(common) {
-  let c = common;
-  if (c[0] - c[1] === c[1] - c[2]) {
-    return c.pop() + c[1] - c[0];
-  } else {
-    return c.pop() * (c[1] / c[0]);
-  }
-}
+const solution = (num) => (num % 2 ? "Odd" : "Even");
