b7fc4c54b6b14b26afa460971e4cc87912110d73
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-06 11:52:43 +0900

    add:치킨 쿠폰

--- cote.js
+++ cote.js
@@ -1,34 +1,20 @@
 /*
 
 문제 설명
-머쓱이는 프로그래머스에 로그인하려고 합니다. 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때, 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
 
-아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
-로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
 제한사항
-회원들의 아이디는 문자열입니다.
-회원들의 아이디는 알파벳 소문자와 숫자로만 이루어져 있습니다.
-회원들의 패스워드는 숫자로 구성된 문자열입니다.
-회원들의 비밀번호는 같을 수 있지만 아이디는 같을 수 없습니다.
-id_pw의 길이는 2입니다.
-id_pw와 db의 원소는 [아이디, 패스워드] 형태입니다.
-1 ≤ 아이디의 길이 ≤ 15
-1 ≤ 비밀번호의 길이 ≤ 6
-1 ≤ db의 길이 ≤ 10
-db의 원소의 길이는 2입니다.
+chicken은 정수입니다.
+0 ≤ chicken ≤ 1,000,000
 
 
 */
 
-function solution(id_pw, db) {
-  var answer = "";
-  let isWrong = false;
-  answer = db.filter((p) => {
-    if (id_pw[0] === p[0] && id_pw[1] === p[1]) return true;
-    if (id_pw[0] === p[0] && id_pw[1] !== p[1]) isWrong = true;
-    return false;
-  }).length
-    ? "login"
-    : "fail";
-  return isWrong ? "wrong pw" : answer;
+function solution(chicken) {
+  let answer = 0;
+  while (chicken >= 10) {
+    answer += Math.floor(chicken / 10);
+    chicken = (chicken % 10) + Math.floor(chicken / 10);
+  }
+  return answer;
 }
