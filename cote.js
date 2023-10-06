fc1591283684d249e49b9bfe241c7e9b62990f6b
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-06 11:54:05 +0900

    add:A로 B 만들기

--- cote.js
+++ cote.js
@@ -1,17 +1,18 @@
 /*
 
 문제 설명
-이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
+문자열 before와 after가 매개변수로 주어질 때, before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-return 값은 이진수를 의미하는 문자열입니다.
-1 ≤ bin1, bin2의 길이 ≤ 10
-bin1과 bin2는 0과 1로만 이루어져 있습니다.
-bin1과 bin2는 "0"을 제외하고 0으로 시작하지 않습니다.
+0 < before의 길이 == after의 길이 < 1,000
+before와 after는 모두 소문자로 이루어져 있습니다.
 
 
 
 */
 
-const solution = (bin1, bin2) =>
-  (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+function solution(before, after) {
+  return before.split("").sort().join("") === after.split("").sort().join("")
+    ? 1
+    : 0;
+}
