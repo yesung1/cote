6ac4f77e503413086df8899ab9deb56a5531172a
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-10 13:10:25 +0900

    add:문자열을 정수로 바꾸기

--- cote.js
+++ cote.js
@@ -1,15 +1,15 @@
 /*
 
 문제 설명
-자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
-예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
 
-제한사항
-N의 범위 : 100,000,000 이하의 자연수
-
+제한 조건
+s의 길이는 1 이상 5이하입니다.
+s의 맨앞에는 부호(+, -)가 올 수 있습니다.
+s는 부호와 숫자로만 이루어져있습니다.
+s는 "0"으로 시작하지 않습니다.
 
 
 */
 
-const solution = (n) =>
-  (answer = [...n.toString()].reduce((acc, curr) => acc + Number(curr), 0));
+const solution = (s) => Number(s);
