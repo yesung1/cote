a0fe91dc6f6fa271878cdc6eea0316709ee3ddbd
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-10 11:00:04 +0900

    add:약수의 합

--- cote.js
+++ cote.js
@@ -1,14 +1,19 @@
 /*
 
 문제 설명
-정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
 
-제한사항
-arr은 길이 1 이상, 100 이하인 배열입니다.
-arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+제한 사항
+n은 0 이상 3000이하인 정수입니다.
 
 
 
 */
 
-const solution = (arr) => arr.reduce((acc, curr) => acc + curr) / arr.length;
+function solution(n) {
+  var answer = 0;
+  for (let x = 0; x <= n; x++) {
+    if (n % x === 0) answer += x;
+  }
+  return answer;
+}
