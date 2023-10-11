d9cafc28ffed6090ea415d833ff335f5bc5d4365
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-11 15:02:04 +0900

    add:제일 작은 수 제거하기

--- cote.js
+++ cote.js
@@ -1,13 +1,13 @@
 /*
 
 문제 설명
-0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
 
-제한사항
-1 ≤ numbers의 길이 ≤ 9
-0 ≤ numbers의 모든 원소 ≤ 9
-numbers의 모든 원소는 서로 다릅니다.
+제한 조건
+arr은 길이 1 이상인 배열입니다.
+인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
 
 */
 
-const solution = (numbers) => 45 - numbers.reduce((acc, curr) => acc + curr);
+const solution = (arr) =>
+  arr.length > 1 ? arr.filter((n) => n !== Math.min(...arr)) : [-1];
