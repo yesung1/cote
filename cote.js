3e65965b234bd37db85cf72311ca8f804931eb5b
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-11 18:28:56 +0900

    add:약수의 개수와 덧셈

--- cote.js
+++ cote.js
@@ -1,18 +1,23 @@
 /*
 
 문제 설명
-길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
-
-이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
+두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
 
 제한사항
-a, b의 길이는 1 이상 1,000 이하입니다.
-a, b의 모든 수는 -1,000 이상 1,000 이하입니다.
+1 ≤ left ≤ right ≤ 1,000
 
 */
 
-function solution(a, b) {
+function solution(left, right) {
   var answer = 0;
-  for (let i = 0; i < a.length; i++) answer += a[i] * b[i];
+  for (let x = left; x <= right; x++) {
+    let count = 0;
+    for (let y = 0; y <= x; y++) {
+      if (x % y === 0) {
+        count++;
+      }
+    }
+    count % 2 ? (answer -= x) : (answer += x);
+  }
   return answer;
 }
