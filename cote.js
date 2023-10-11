b36edbf6018738c1ddad3ebc86637bfb1a849937
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-11 10:18:23 +0900

    add:문자열 내 p와 y의 개수

--- cote.js
+++ cote.js
@@ -1,15 +1,25 @@
 /*
 
 문제 설명
-문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
 
-제한 조건
-s의 길이는 1 이상 5이하입니다.
-s의 맨앞에는 부호(+, -)가 올 수 있습니다.
-s는 부호와 숫자로만 이루어져있습니다.
-s는 "0"으로 시작하지 않습니다.
+예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+제한사항
+문자열 s의 길이 : 50 이하의 자연수
+문자열 s는 알파벳으로만 이루어져 있습니다.
 
 
 */
 
-const solution = (s) => Number(s);
+function solution(s) {
+  var answer = true;
+  let pNum = 0;
+  let yNum = 0;
+  answer = [...s.toUpperCase()].map((n) => {
+    if (n === "P") pNum++;
+    if (n === "Y") yNum++;
+  });
+  pNum === yNum ? (answer = true) : (answer = false);
+  return answer;
+}
