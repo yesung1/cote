c97a582ada6ebb6a25c044ffa56bc4ffa39e17be
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-12 10:58:17 +0900

    update:부족한 금액 계산하기

--- cote.js
+++ cote.js
@@ -13,7 +13,6 @@
 */
 
 function solution(price, money, count) {
-  const totalCost = (price * count * (count + 1)) / 2;
-  const shortfall = totalCost - money;
-  return Math.max(0, shortfall);
+  var answer = (price * count * (count + 1)) / 2 - money;
+  return answer >= 0 ? answer : 0;
 }
