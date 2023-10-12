7dc76b60825afa09383137c79d2c7192ec7330e2
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-12 10:55:37 +0900

    add:부족한 금액 계산하기

--- cote.js
+++ cote.js
@@ -1,12 +1,19 @@
 /*
 
 문제 설명
-문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
-s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다. 이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다. 즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다.
+놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
+단, 금액이 부족하지 않으면 0을 return 하세요.
 
-제한 사항
-str은 길이 1 이상인 문자열입니다.
+제한사항
+놀이기구의 이용료 price : 1 ≤ price ≤ 2,500, price는 자연수
+처음 가지고 있던 금액 money : 1 ≤ money ≤ 1,000,000,000, money는 자연수
+놀이기구의 이용 횟수 count : 1 ≤ count ≤ 2,500, count는 자연수
 
 */
 
-const solution = (s) => s.split("").sort().reverse().join("");
+function solution(price, money, count) {
+  const totalCost = (price * count * (count + 1)) / 2;
+  const shortfall = totalCost - money;
+  return Math.max(0, shortfall);
+}
