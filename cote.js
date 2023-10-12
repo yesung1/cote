25a006daa10f09cc654ac96d56d3d7265b4c9ff5
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-12 09:48:14 +0900

    add:문자열 내림차순으로 배치하기

--- cote.js
+++ cote.js
@@ -1,23 +1,12 @@
 /*
 
 문제 설명
-두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
 
-제한사항
-1 ≤ left ≤ right ≤ 1,000
+제한 사항
+str은 길이 1 이상인 문자열입니다.
 
 */
 
-function solution(left, right) {
-  var answer = 0;
-  for (let x = left; x <= right; x++) {
-    let count = 0;
-    for (let y = 0; y <= x; y++) {
-      if (x % y === 0) {
-        count++;
-      }
-    }
-    count % 2 ? (answer -= x) : (answer += x);
-  }
-  return answer;
-}
+const solution = (s) => s.split("").sort().reverse().join("");
