48dbfd71c2cf2762282d780abe3aecac531cb7d3
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-16 16:51:21 +0900

    add:시저 암호

--- cote.js
+++ cote.js
@@ -2,36 +2,30 @@
 
 
 문제 설명
-명함 지갑을 만드는 회사에서 지갑의 크기를 정하려고 합니다. 다양한 모양과 크기의 명함들을 모두 수납할 수 있으면서, 작아서 들고 다니기 편한 지갑을 만들어야 합니다. 이러한 요건을 만족하는 지갑을 만들기 위해 디자인팀은 모든 명함의 가로 길이와 세로 길이를 조사했습니다.
+어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
 
-아래 표는 4가지 명함의 가로 길이와 세로 길이를 나타냅니다.
+제한 조건
+공백은 아무리 밀어도 공백입니다.
+s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
+s의 길이는 8000이하입니다.
+n은 1 이상, 25이하인 자연수입니다.
 
-명함 번호	가로 길이	세로 길이
-1	60	50
-2	30	70
-3	60	30
-4	80	40
-가장 긴 가로 길이와 세로 길이가 각각 80, 70이기 때문에 80(가로) x 70(세로) 크기의 지갑을 만들면 모든 명함들을 수납할 수 있습니다. 하지만 2번 명함을 가로로 눕혀 수납한다면 80(가로) x 50(세로) 크기의 지갑으로 모든 명함들을 수납할 수 있습니다. 이때의 지갑 크기는 4000(=80 x 50)입니다.
-
-모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다. 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
-
-제한사항
-sizes의 길이는 1 이상 10,000 이하입니다.
-sizes의 원소는 [w, h] 형식입니다.
-w는 명함의 가로 길이를 나타냅니다.
-h는 명함의 세로 길이를 나타냅니다.
-w와 h는 1 이상 1,000 이하인 자연수입니다.
 
 
 */
 
-function solution(sizes) {
-  let mW = 0,
-    mH = 0;
-  for (let i = 0; i < sizes.length; i++) {
-    let [w, h] = sizes[i].sort((a, b) => b - a);
-    mW = Math.max(mW, w);
-    mH = Math.max(mH, h);
-  }
-  return mW * mH;
+function solution(s, n) {
+  var answer = "";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  let temp = 0;
+
+  answer = [...s].map((i) => {
+    if (i === " ") return i;
+    i === i.toUpperCase()
+      ? (temp = upper.charAt((upper.indexOf(i) + n) % upper.length))
+      : (temp = lower.charAt((lower.indexOf(i) + n) % lower.length));
+    return temp;
+  });
+  return answer.join("");
 }
