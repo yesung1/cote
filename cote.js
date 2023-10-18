42b7be633a03dee06342b529d2e6db0db5d1ed7a
yesung1 <67320461+yesung1@users.noreply.github.com>
2023-10-18 16:59:26 +0900

    add:2016년

--- cote.js
+++ cote.js
@@ -2,38 +2,19 @@
 
 
 문제 설명
-코니는 영어 단어가 적힌 카드 뭉치 두 개를 선물로 받았습니다. 코니는 다음과 같은 규칙으로 카드에 적힌 단어들을 사용해 원하는 순서의 단어 배열을 만들 수 있는지 알고 싶습니다.
+2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
 
-원하는 카드 뭉치에서 카드를 순서대로 한 장씩 사용합니다.
-한 번 사용한 카드는 다시 사용할 수 없습니다.
-카드를 사용하지 않고 다음 카드로 넘어갈 수 없습니다.
-기존에 주어진 카드 뭉치의 단어 순서는 바꿀 수 없습니다.
-예를 들어 첫 번째 카드 뭉치에 순서대로 ["i", "drink", "water"], 두 번째 카드 뭉치에 순서대로 ["want", "to"]가 적혀있을 때 ["i", "want", "to", "drink", "water"] 순서의 단어 배열을 만들려고 한다면 첫 번째 카드 뭉치에서 "i"를 사용한 후 두 번째 카드 뭉치에서 "want"와 "to"를 사용하고 첫 번째 카드뭉치에 "drink"와 "water"를 차례대로 사용하면 원하는 순서의 단어 배열을 만들 수 있습니다.
+입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
 
-문자열로 이루어진 배열 cards1, cards2와 원하는 단어 배열 goal이 매개변수로 주어질 때, cards1과 cards2에 적힌 단어들로 goal를 만들 있다면 "Yes"를, 만들 수 없다면 "No"를 return하는 solution 함수를 완성해주세요.
-
-제한사항
-1 ≤ cards1의 길이, cards2의 길이 ≤ 10
-1 ≤ cards1[i]의 길이, cards2[i]의 길이 ≤ 10
-cards1과 cards2에는 서로 다른 단어만 존재합니다.
-2 ≤ goal의 길이 ≤ cards1의 길이 + cards2의 길이
-1 ≤ goal[i]의 길이 ≤ 10
-goal의 원소는 cards1과 cards2의 원소들로만 이루어져 있습니다.
-cards1, cards2, goal의 문자열들은 모두 알파벳 소문자로만 이루어져 있습니다.
+제한 조건
+2016년은 윤년입니다.
+2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
 
 
 */
 
-function solution(cards1, cards2, goal) {
-  var answer = "Yes";
-  for (let x of goal) {
-    if (cards1[0] === x) {
-      cards1.shift();
-    } else if (cards2[0] === x) {
-      cards2.shift();
-    } else {
-      answer = "No";
-    }
-  }
-  return answer;
+function solution(a, b) {
+  let d = new Date(`2016-${a}-${b}`).getDay();
+  let dName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  return dName[d];
 }
